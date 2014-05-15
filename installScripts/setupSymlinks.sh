90129af7c1515a1d49980bd8a5c727c4873dd817
Tehforsch <Tonipeter92@googlemail.com>
2014-05-15 10:46:51 +0200

    Added install script to add all the necessary symlinks on a new computer

---
+++ installScripts/setupSymlinks.sh
@@ -0,0 +1,11 @@
+ln -s ~/.usrconfig/.vimrc ~/.vimrc
+ln -s ~/.usrconfig/.zshrc ~/.zshrc
+ln -s ~/.usrconfig/.gitconfig ~/.gitconfig
+ln -s ~/.usrconfig/.gitignoreGlobal ~/.gitignoreGlobal
+ln -s ~/.usrconfig/.gnuplot ~/.gnuplot
+rm -r ~/.vim/
+ln -s ~/.usrconfig/.vim ~/.vim/
+ln -s ~/.usrconfig/.Xmodmap ~/.Xmodmap
+ln -s ~/.usrconfig/.Xmodmap ~/.Xmodmap
+ln -s ~/.usrconfig/.i3/config ~/.i3/config
+ln -s ~/.usrconfig/.i3status.conf ~/.i3status.conf
