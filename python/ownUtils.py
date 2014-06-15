92eae8bc4b04b9d98ed068407dad74abb36aa290
Tehforsch <Tonipeter92@googlemail.com>
2014-06-15 11:54:29 +0200

    Added functions to read data files in ownUtils

--- python/ownUtils.py
+++ python/ownUtils.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plot
+import numpy as np
 
 def readFile(fname):
     """Returns the lines contained in fname in standard list format"""
@@ -6,6 +6,30 @@ def readFile(fname):
     lines = f.readlines()
     f.close()
     return lines
+
+def writeFile(fname, content):
+    """Writes the content into a file of path fname"""
+    f = open(fname, "w")
+    f.write(content)
+    f.close()
+
+def readDataFile(fname, sep = " "):
+    """Reads the file fname and returns a list of numpy arrays 
+    where each array corresponds to a line in the file
+    and each entry is separated by sep (default : " ")"""
+    lines = readFile(fname)
+    data = []
+    for l in lines:
+        splitted = l.replace("\n", "").split(sep)
+        data.append(list(map(float, splitted)))
+    return list(map(np.array, data))
+
+def writeDataFile(fname, data, sep = " "):
+    """Writes the data to the file fname, separating each list with
+    a line break and each entry with sep (default " ")"""
+    strData = map(lambda x : map(str, x), data)
+    s = "\n".join(map(sep.join, strData))
+    writeFile(fname, s)
 
 def charactersBetween(string, beginning, end):
     """Returns all characters in a string that are contained between two characters beginning and end"""
@@ -21,16 +45,16 @@ def charactersBetween(string, beginning, end):
             collected = collected + z
     return None
 
-def plotData(pairs):
-    """Uses pyplot to quickly plot a list of (x, y) pairs"""
-    xs = []
-    ys = []
-    for pair in pairs:
-        xs.append(pair[0])
-        ys.append(pair[1])
-    plot.plot(xs, ys, 'ro')
-    #plot.axis([0, 6, 0, 20])
-    plot.show()
+# def plotData(pairs):
+#     """Uses pyplot to quickly plot a list of (x, y) pairs"""
+#     xs = []
+#     ys = []
+#     for pair in pairs:
+#         xs.append(pair[0])
+#         ys.append(pair[1])
+#     plot.plot(xs, ys, 'ro')
+#     #plot.axis([0, 6, 0, 20])
+#     plot.show()
 
 def transpose(lst):
     """Returns the transposed (rectangular) list"""
