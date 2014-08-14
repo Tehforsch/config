9b9cc331f9ec28fcf690adedeef73ebd5c9c63c7
Tehforsch <Tonipeter92@googlemail.com>
2014-08-14 11:11:59 +0200

    Added volume increase script which caps before it distorts (over 100%)

---
+++ scripts/increaseVolume.sh
@@ -0,0 +1,11 @@
+#Get volume of the pulse audio source $id
+id=0
+volume=$(pactl list | ~/.bin/ack Volume | head -n $(expr 1 + $id) | ~/.bin/ack "Volume: 0:[\s]* ([\d]*)%" --output=\$1)
+echo "Current volume : $volume"
+#If volume is > 100-increase (that means no overdrive going on if we increase by increase%) then increase the volume
+increase=10
+maxVolume=100
+maxVolume=$(expr $maxVolume - $increase)
+if [ $volume -le $maxVolume ]; then
+    pactl set-sink-volume $id -- +$increase%
+fi
