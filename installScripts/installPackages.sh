ba5a0b923f14c3d281bb8bbf9aae2a34015ee26c
Tehforsch <Tonipeter92@googlemail.com>
2014-09-28 16:50:16 +0200

    Added scrot to install packages

--- installScripts/installPackages.sh
+++ installScripts/installPackages.sh
@@ -1,2 +1,2 @@
-apt-get install vim-gtk python ruby ruby-dev vlc i3 python-pip curl zsh okular gnuplot autojump python-dev python3-dev texlive texlive-latex-extra
+apt-get install vim-gtk python ruby ruby-dev vlc i3 python-pip curl zsh okular gnuplot autojump python-dev python3-dev texlive texlive-latex-extra scrot
 
