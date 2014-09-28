034b96a6ae3d6d44adf48496c4d6e55bed9a6098
Tehforsch <Tonipeter92@googlemail.com>
2014-09-28 16:08:10 +0200

    Added latex (including extra packages) to install script

--- installScripts/installPackages.sh
+++ installScripts/installPackages.sh
@@ -1,1 +1,2 @@
-apt-get install vim-gtk python ruby ruby-dev vlc chromium-browser i3 python-pip curl zsh okular gnuplot autojump python-dev python3-dev
+apt-get install vim-gtk python ruby ruby-dev vlc i3 python-pip curl zsh okular gnuplot autojump python-dev python3-dev texlive texlive-latex-extra
+
