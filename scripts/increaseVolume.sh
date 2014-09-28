a2b0657b7225257bc0127d4bd13d49b38f0fc1a7
Tehforsch <Tonipeter92@googlemail.com>
2014-09-28 16:08:36 +0200

    Made increaseVolume work properly for ids != 0

--- scripts/increaseVolume.sh
+++ scripts/increaseVolume.sh
@@ -1,11 +1,12 @@
 #Get volume of the pulse audio source $id
 id=1
-volume=$(pactl list | ~/.bin/ack Volume | head -n $(expr 1 + $id) | ~/.bin/ack "Volume: 0:[\s]* ([\d]*)%" --output=\$1)
+line=$(expr 1 + $id + $id)
+volume=$(pactl list | ~/.bin/ack Volume | head -n $line | tail -n 1 | ~/.bin/ack "Volume: 0:[\s]* ([\d]*)%" --output=\$1)
 echo "Current volume : $volume"
 #If volume is > 100-increase (that means no overdrive going on if we increase by increase%) then increase the volume
 increase=10
-maxVolume=100
+maxVolume=90
 maxVolume=$(expr $maxVolume - $increase)
-if [ $volume -le $maxVolume ]; then
+if [ "$volume" -le "$maxVolume" ]; then
     pactl set-sink-volume $id -- +$increase%
 fi
