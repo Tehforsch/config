02f37be72f3a619eacaa846fa8e13c17fe253501
Tehforsch <Tonipeter92@googlemail.com>
2014-11-15 12:13:15 +0100

    "Some bugfixes to this horrible script hopefully get it to work finally"

--- scripts/changeVolume.sh
+++ scripts/changeVolume.sh
@@ -1,17 +1,25 @@
+#!/bin/bash
 #Get volume of the pulse audio source $id
-id=0
-increase=10
+for id in $(seq 0 5)
+do
+    increase=10
 
-if [ $1 -eq 0 ]; then
-    pactl set-sink-volume $id -- -$increase%
-else
-    line=$(expr 1 + $id + $id)
-    volume=$(pactl list | ~/.bin/ack Volume | head -n $line | tail -n 1 | ~/.bin/ack "Volume: 0:[\s]* ([\d]*)%" --output=\$1)
-    echo "Current volume : $volume"
-    #If volume is > 100-increase (that means no overdrive going on if we increase by increase%) then increase the volume
-    maxVolume=90
-    maxVolume=$(expr $maxVolume - $increase)
-    if [ "$volume" -le "$maxVolume" ]; then
-        pactl set-sink-volume $id -- +$increase%
+    if [[ $1 -eq 0 ]]; then
+        pactl set-sink-volume $id -- -$increase%
+    else
+        line=$(expr 1 + $id + $id)
+        volume=$(pactl list | ~/.bin/ack Volume | head -n $line | tail -n 1 | ~/.bin/ack "Volume: 0:[\s]* ([\d]*)%" --output=\$1)
+        echo "Current volume : $volume"
+        #If volume is > 100-increase (that means no overdrive going on if we increase by increase%) then increase the volume
+        maxVolume=100
+        maxVolume=$(expr $maxVolume - $increase)
+        if [[ "$volume" -le "$maxVolume" ]]; then
+            echo $volume"hi"
+            pactl set-sink-volume $id -- +$increase%
+            echo "pactl set-sink-volume $id -- +$increase%"
+        else
+            echo $volume"ho"
+            increase=$(expr 100 - $volume)
+        fi
     fi
-fi
+done
