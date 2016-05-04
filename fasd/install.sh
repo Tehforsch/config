780b091a5deb1901dde90e21c96e52b6a6ea3211
Tehforsch <Tonipeter92@googlemail.com>
2016-05-04 21:24:25 +0200

    fasd is installed via zgen

--- fasd/install.sh
+++
@@ -1,7 +0,0 @@
-# This script is most likely really horrible.
-curl https://codeload.github.com/clvv/fasd/legacy.zip/1.0.1 > fasd.zip
-unzip fasd.zip -d fasd
-cd fasd/clvv-fasd-4822024
-make install
-cd ..
-rm -rf fasd
