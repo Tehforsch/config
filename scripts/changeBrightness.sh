fab3f5d75cf623f23924d346a209fafdac0488d6
Tehforsch <Tonipeter92@googlemail.com>
2016-05-18 23:05:44 +0200

    add ctrl+shift+b binding to toggle brightness of laptop screen between 100 and 0 (mainly for watching movies)

--- scripts/changeBrightness.sh
+++ scripts/changeBrightness.sh
@@ -3,9 +3,16 @@ if [ $1 -eq 1 ]; then
 fi
 brightFloat=$(xbacklight)
 brightInt=${brightFloat%.*}
-if [ $1 -eq 0 ]; then
+if [ $1 -eq -1 ]; then
     if [ $brightInt -gt 10 ]; then
         xbacklight -dec 10
+    fi
+fi
+if [ $1 -eq 0 ]; then
+    if [ $brightInt -eq 0 ]; then
+        xbacklight -set 100
+    else
+        xbacklight -set 0
     fi
 fi
 echo $(xbacklight)
