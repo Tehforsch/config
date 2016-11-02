3bb1e789aee00c82847774f4ba22a7ffe3405b0b
Tehforsch <Tonipeter92@googlemail.com>
2016-11-02 19:54:47 +0100

    use streamlink instead of livestreamer

--- scripts/startLivestreamer.sh
+++ scripts/startLivestreamer.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-# Simply start livestreamer on the given stream name in best quality and detach the process from the terminal
+# Simply start streamlink on the given stream name in best quality and detach the process from the terminal
 
 if [ $# -ne 1 ]; then
     echo "Missing parameter : stream name"
 else
-    # I cannot use (only) &! here because it gets handled differently than nohup and will actually kill livestreamer.
-    nohup livestreamer twitch.tv/$1 best > /dev/null 2>&1 &
+    # I cannot use (only) &! here because it gets handled differently than nohup and will actually kill streamlink.
+    nohup streamlink --twitch-oauth-token w61oe302b2dya5ki9zzvcpqqmqlp3o twitch.tv/$1 best 
 fi
