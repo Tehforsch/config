5e02efd5c42e8040207e450f904ca2356b83ee0c
Tehforsch <Tonipeter92@googlemail.com>
2017-01-23 23:08:45 +0100

    Try adding redshift to startup scripts

--- startupScripts/runAll.sh
+++ startupScripts/runAll.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ~/projects/config/startupScripts/screenConfiguration.sh
 ~/projects/config/startupScripts/keyboardConfiguration.sh
+redshift -l 48.7:9.18 -t 5700:3600 -g 0.8 -m randr -v
