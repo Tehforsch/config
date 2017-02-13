5858e962ac8cf861364aa25148aecb407e6b1228
Tehforsch <Tonipeter92@googlemail.com>
2017-02-13 11:20:24 +0100

    fix redshift being executed forever when doing alt+shift+o

--- startupScripts/runAll.sh
+++ startupScripts/runAll.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 ~/projects/config/startupScripts/screenConfiguration.sh
 ~/projects/config/startupScripts/keyboardConfiguration.sh
-redshift -l 48.7:9.18 -t 5700:3600 -g 0.8 -m randr -v
