90052791cd8253db4f7cb6eaed1574b27203c15d
Tehforsch <Tonipeter92@googlemail.com>
2017-02-25 14:10:10 +0100

    change to neovim

--- installScripts/installExternalPPA.sh
+++ installScripts/installExternalPPA.sh
@@ -4,6 +4,8 @@ sudo add-apt-repository ppa:git-core/ppa
 #Spotify
 #add-apt-repository 'deb http://repository.spotify.com/ stable non-free' 
 #apt-key adv --recv-keys --keyserver keyserver.ubuntu.com D2C19886  
+#Neovim
+add-apt-repository ppa:neovim-ppa/unstable
 
 #Ledger
 add-apt-repository ppa:mbudde/ledger
@@ -12,5 +14,5 @@ add-apt-repository ppa:mbudde/ledger
 apt-get update
 
 # Install everything
-apt-get --yes install ledger spotify-client git
+apt-get --yes install ledger spotify-client git neovim
 apt-get --yes install -f
---
+++ vim/installNvimrc.sh
@@ -0,0 +1,2 @@
+mkdir ~/.config/nvim
+ln -s $CONFIG/vim/vimrc.symlink $HOME/.config/nvim/init.vim
