32dc30a53e34282945b3565b943d77ab041c79c2
Tehforsch <Tonipeter92@googlemail.com>
2017-11-29 12:06:08 +0100

    remove unused scripts

--- scripts/addToTodo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [ $# != 2 ]; then
-    echo "Needs two arguments: todo folder and task"
-    exit 1
-fi
-echo "$2" >> "$HOME/resource/todo/$1/nextActions"
--- scripts/openReport.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/zsh
-okular $MASTER/simulations/$1/report/main.pdf
