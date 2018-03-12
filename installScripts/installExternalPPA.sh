21a96903c9943dc73f2217799e87f45ca0f3996b
Tehforsch <Tonipeter92@googlemail.com>
2018-03-12 10:53:47 +0100

    update for hlrs

--- installScripts/installExternalPPA.sh
+++ installScripts/installExternalPPA.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository ppa:git-core/ppa
 #add-apt-repository 'deb http://repository.spotify.com/ stable non-free' 
 #apt-key adv --recv-keys --keyserver keyserver.ubuntu.com D2C19886  
 #Neovim
-add-apt-repository ppa:neovim-ppa/unstable
+# add-apt-repository ppa:neovim-ppa/unstable
 
 #Ledger
 add-apt-repository ppa:mbudde/ledger
@@ -14,5 +14,6 @@ add-apt-repository ppa:mbudde/ledger
 apt-get update
 
 # Install everything
-apt-get --yes install ledger spotify-client git neovim
+# apt-get --yes install ledger spotify-client git neovim
+apt-get --yes install spotify-client git
 apt-get --yes install -f
