69aff52e2f80215a93f0b83f6b8581b3d770b30d
Tehforsch <Tonipeter92@googlemail.com>
2019-07-18 14:27:31 +0200

    fix todoist script so it allows for project specification via argparse

--- scripts/addTodoistTask.py
+++ scripts/addTodoistTask.py
@@ -1,17 +1,27 @@
 from pytodoist import todoist
+import argparse
 import os
 import sys
 
+defaultProject = "Inbox"
 # to make this work: add the token api from todoist in ~/.todoistToken
 
-defaultProject = "Inbox"
+def addTask(taskName, projectName=None):
+    if projectName is None:
+        projectName = defaultProject
 
-with open(os.path.expanduser("~/.todoistToken"), "r") as f:
-    token = f.readlines()[0].replace("\n", "")
+    with open(os.path.expanduser("~/.todoistToken"), "r") as f:
+        token = f.readlines()[0].replace("\n", "")
 
-user = todoist.login_with_api_token(token)
+    user = todoist.login_with_api_token(token)
+    project = user.get_project(projectName)
+    task = project.add_task(taskName)
 
-project = user.get_project(defaultProject)
 
-taskName = " ".join(sys.argv[1:])
-task = project.add_task(taskName)
+parser = argparse.ArgumentParser(description='Run the given simulation by copying it to the simulation folder along with src and executable and running it.')
+
+parser.add_argument("name", nargs="*", help="Name of the task")
+parser.add_argument("-p", nargs = 1, dest="project", help="Name of the project")
+
+args = parser.parse_args()
+addTask(args.name, args.project[0])
