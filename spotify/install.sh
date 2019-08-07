6b99e69470dfd9825460f812e162d4f0c729d928
Tehforsch <Tonipeter92@googlemail.com>
2019-08-07 12:18:06 +0200

    update spotify installation for 18.04

--- spotify/install.sh
+++ spotify/install.sh
@@ -1,2 +1,3 @@
-# disable the annoying notifications
+snap install spotify
+#disable the annoying notifications
 echo "ui.track_notifications_enabled=false" >> ~/.config/spotify/Users/tehforsch-user/prefs
