79ca4d885c208c7af6079bbc82cde6cd1460a9f9
Tehforsch <Tonipeter92@googlemail.com>
2019-09-06 16:45:28 +0200

    change vundle to vim-plug

---
+++ vimplug/install.sh
@@ -0,0 +1,6 @@
+# For vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# For nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
