4a18cb898cd4a72a249cbb81a0801686a5e71fd7
Tehforsch <Tonipeter92@googlemail.com>
2019-10-11 15:42:24 +0200

    change formatting of queue status output

--- scripts/getJobStatus.sh
+++ scripts/getJobStatus.sh
@@ -4,5 +4,10 @@ for line in $output; do
     jobId=$line
     state=$(/opt/moab/bin/checkjob $jobId | grep State: | head -n 1 | awk '{print $2}')
     wallTime=$(/opt/moab/bin/checkjob $jobId | grep WallTime: | head -n 1 | awk '{print $2}')
-    echo -n "$state($wallTime) "
+    maxTime=$(/opt/moab/bin/checkjob $jobId | grep WallTime: | head -n 1 | awk '{print $4}')
+    if [[ $state == "Idle" ]]; then
+        echo -n "Queue"
+    else
+        echo -n "$state(${wallTime:0:5}/${maxTime:0:5}) "
+    fi
 done
