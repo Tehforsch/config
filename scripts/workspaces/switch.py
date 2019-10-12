481c810e4ea1cda11343e6350c13dcf12cae51b2
Tehforsch <Tonipeter92@googlemail.com>
2019-10-12 15:03:52 +0200

    add awesome new context-based workspace management system which allows to have the usual workspace hotkeys but to switch 'contexts', which might be different projects or work/personal stuff. also allows to have shared workspaces that are the same in every context (music player, email client, chat client, ...)

--- scripts/workspaces/switch.py
+++ scripts/workspaces/switch.py
@@ -2,31 +2,123 @@ import sys
 import argparse
 import os
 from pathlib import Path
+from math import ceil
+import i3
 
-modeFile = Path(os.path.expanduser("~"), ".mode")
-workspaceNameFormat = "{}_{}"
+defaultContext = 0
+contextNames = {
+        0: "a",
+        1: "b",
+        2: "c",
+        3: "d",
+        4: "e",
+}
 
-def setWorkspace(workspaceName):
-    modeName = getMode()
-    os.system("i3-msg workspace {}".format(workspaceNameFormat.format(modeName, workspaceName)))
+sharedWorkspaceNames = ["music"]
+contextFile = Path(os.path.expanduser("~"), ".context")
+workspaceNameFormat = "{workspaceId}:{contextName}{workspaceNumber}"
+sharedWorkspaceNameFormat = "{workspaceId}:{workspaceName}"
 
-def getMode():
-    with modeFile.open("r") as f:
-        return f.readlines()[0].replace("\n", "")
+numSharedWorkspaces = len(sharedWorkspaceNames)
+workspacesPerContext = 12
+offsetPerContext = (ceil(workspacesPerContext / 10)) * 10
+minContextOffset = ceil(numSharedWorkspaces / offsetPerContext)
 
-def setMode(mode):
-    with modeFile.open("w") as f:
-        f.write(mode)
+def getWorkspaceId(contextNumber, workspaceNumber):
+    return offsetPerContext * contextNumber + workspaceNumber
+
+def getNumbersFromId(workspaceId):
+    contextNumber = workspaceId // offsetPerContext
+    workspaceNumber = workspaceId - contextNumber * offsetPerContext
+    assert getWorkspaceId(contextNumber, workspaceNumber) == workspaceId
+    return contextNumber, workspaceNumber
+
+def getFocusedWorkspaceId():
+    workspaces = i3.get_workspaces()
+    for ws in workspaces:
+        if ws['focused'] == True:
+            return ws['num']
+
+def getFocusedWorkspaceNumber():
+    workspaceId = getFocusedWorkspaceId()
+    contextNumber, workspaceNumber = getNumbersFromId(workspaceId)
+    return workspaceNumber
+
+def getContextName(contextNumber):
+    return contextNames[contextNumber]
+
+def createContextFileIfItDoesNotExist():
+    if not contextFile.is_file():
+        with contextFile.open("w") as f:
+            f.write(str(defaultContext))
+
+def getSharedWorkspaceId(sharedWorkspaceNumber):
+    return sharedWorkspaceNames.index(sharedWorkspaceNumber)
+
+def getSharedWorkspaceName(sharedWorkspaceName):
+    assert sharedWorkspaceName in sharedWorkspaceNames
+    workspaceId = getSharedWorkspaceId(sharedWorkspaceName)
+    return sharedWorkspaceNameFormat.format(workspaceId=workspaceId, workspaceName=sharedWorkspaceName)
+
+def getWorkspaceName(workspaceIdentifier):
+    if workspaceIdentifier in sharedWorkspaceNames:
+        return getSharedWorkspaceName(workspaceIdentifier)
+    else:
+        return getNonSharedWorkspaceName(int(workspaceIdentifier))
+
+def getNonSharedWorkspaceName(workspaceNumber):
+    contextNumber = readContext()
+    contextName = getContextName(contextNumber)
+    workspaceId = getWorkspaceId(contextNumber, workspaceNumber)
+    workspaceName = workspaceNameFormat.format(contextName=contextName, workspaceNumber=workspaceNumber, workspaceId=workspaceId)
+    return workspaceName
+
+def switchWorkspace(workspaceName):
+    os.system("i3-msg workspace {}".format(workspaceName))
+
+def moveToWorkspace(workspaceName):
+    os.system("i3-msg move workspace {}".format(workspaceName))
+    switchWorkspace(workspaceName)
+
+def readContext():
+    with contextFile.open("r") as f:
+        return int(f.read())
+
+def switchContext(context):
+    setContext(context)
+    focusedWorkspaceNumber = getFocusedWorkspaceNumber()
+    switchWorkspace(getWorkspaceName(focusedWorkspaceNumber))
+
+def moveToContext(context):
+    setContext(context)
+    focusedWorkspaceNumber = getFocusedWorkspaceNumber()
+    workspaceName = getWorkspaceName(focusedWorkspaceNumber)
+    moveToWorkspace(workspaceName)
+    switchWorkspace(workspaceName)
+
+def setContext(context):
+    with contextFile.open("w") as f:
+        f.write(str(context))
 
 def setupArgs():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--mode', default=None, type=str, help="Switch to given mode")
-    parser.add_argument('--workspace', default=None, type=str, help="Switch to given workspace")
+    parser.add_argument('--switchContext', default=None, type=int, help="Switch to given context")
+    parser.add_argument('--moveToContext', default=None, type=int, help="Move the focused window to the corresponding workspace in another context")
+    parser.add_argument('--switchWorkspace', default=None, type=str, help="Switch to given workspace number")
+    parser.add_argument('--moveToWorkspace', default=None, type=str, help="Move the focused window to the given workspace number")
     args = parser.parse_args()
     return args
 
-args = setupArgs()
-if args.mode is not None:
-    setMode(args.mode)
-if args.workspace is not None:
-    setWorkspace(args.workspace)
+def main():
+    createContextFileIfItDoesNotExist()
+    args = setupArgs()
+    if args.switchContext is not None:
+        switchContext(args.switchContext)
+    if args.moveToContext is not None:
+        moveToContext(args.moveToContext)
+    if args.switchWorkspace is not None:
+        switchWorkspace(getWorkspaceName(args.switchWorkspace))
+    if args.moveToWorkspace is not None:
+        moveToWorkspace(getWorkspaceName(args.moveToWorkspace))
+
+main()
