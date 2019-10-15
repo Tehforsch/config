19a2dc7c1b90eb88a2e7c01f8ef8d5cf62fc798d
Tehforsch <Tonipeter92@googlemail.com>
2019-10-15 14:21:45 +0200

    make job output nicer

--- scripts/getJobStatus.sh
+++ scripts/getJobStatus.sh
@@ -1,3 +1,9 @@
+function stripSeconds {
+    colonIndex=$(expr index $1 ":")
+    secondColonIndex=$(($colonIndex + $(expr index ${1:$colonIndex:100} ":")))
+    echo ${1:0:$(($secondColonIndex - 1))}
+}
+
 # can't use showq or checkjob via ssh because its an alias, so use full path
 output=$(/opt/moab/bin/showq | grep $USER | tr -s " " | cut -f 1 -d " ")
 for line in $output; do
@@ -5,9 +11,11 @@ for line in $output; do
     state=$(/opt/moab/bin/checkjob $jobId | grep State: | head -n 1 | awk '{print $2}')
     wallTime=$(/opt/moab/bin/checkjob $jobId | grep WallTime: | head -n 1 | awk '{print $2}')
     maxTime=$(/opt/moab/bin/checkjob $jobId | grep WallTime: | head -n 1 | awk '{print $4}')
+    colonIndex=$(expr index wallTime ":")
     if [[ $state == "Idle" ]]; then
         echo -n "Queue"
     else
-        echo -n "$state(${wallTime:0:5}/${maxTime:0:5}) "
+        echo -n "$state($(stripSeconds $wallTime)/$(stripSeconds $maxTime)) "
     fi
 done
+
