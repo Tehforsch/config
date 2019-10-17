d900cf1e1ef9a6536868d06cafe9bbd1af168636
Tehforsch <Tonipeter92@googlemail.com>
2019-10-17 16:51:03 +0200

    nicer display for multiple jobs

--- scripts/getJobStatus.sh
+++ scripts/getJobStatus.sh
@@ -6,6 +6,7 @@ function stripSeconds {
 
 # can't use showq or checkjob via ssh because its an alias, so use full path
 output=$(/opt/moab/bin/showq | grep $USER | tr -s " " | cut -f 1 -d " ")
+echo -n " Jobs: "
 for line in $output; do
     jobId=$line
     state=$(/opt/moab/bin/checkjob $jobId | grep State: | head -n 1 | awk '{print $2}')
@@ -13,7 +14,7 @@ for line in $output; do
     maxTime=$(/opt/moab/bin/checkjob $jobId | grep WallTime: | head -n 1 | awk '{print $4}')
     colonIndex=$(expr index wallTime ":")
     if [[ $state == "Idle" ]]; then
-        echo -n "Queue"
+        echo -n "Queueing "
     elif [[ $state == "Running" ]]; then
         echo -n "Run($(stripSeconds $wallTime)/$(stripSeconds $maxTime)) "
     fi
