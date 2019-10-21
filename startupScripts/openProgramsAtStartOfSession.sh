6ec59a2ba88a1031ff2c2057c7fa67c10ef2184d
Tehforsch <Tonipeter92@googlemail.com>
2019-10-21 22:57:30 +0200

    use local version of i3Contexts

--- startupScripts/openProgramsAtStartOfSession.sh
+++ startupScripts/openProgramsAtStartOfSession.sh
@@ -9,17 +9,17 @@ i3-msg append_layout ~/projects/config/i3/restore/telegram.json
 # Open todoist
 i3-msg workspace 2:todo
 i3-msg append_layout ~/projects/config/i3/restore/todoist.json
-$scripts/startChrome.sh --new-window todoist.com
+$scripts/startChrome.sh --new-window todoist.com&
 
 # Open Gmail + (Thunderbird)
 i3-msg workspace 3:mail
 if [[ $systemName == "ita" ]]; then
     i3-msg append_layout ~/projects/config/i3/restore/thunderbird.json
-    thunderbird
+    thunderbird&
 fi
 i3-msg append_layout ~/projects/config/i3/restore/gmail.json
-$scripts/startChrome.sh --new-window mail.google.com
+$scripts/startChrome.sh --new-window mail.google.com&
 
 # Spotify wont listen anyways because its obviously a garbage program written by garbage people so just go to the workspace and open it
 i3-msg workspace 1:music
-spotify
+spotify&
