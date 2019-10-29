412f8451f03ad8003c05a593dec8c5f83c3ca4b8
Tehforsch <Tonipeter92@googlemail.com>
2019-10-29 12:14:36 +0100

    add sleep to open program script so it hopefully gets the windows right

--- startupScripts/openProgramsAtStartOfSession.sh
+++ startupScripts/openProgramsAtStartOfSession.sh
@@ -11,6 +11,8 @@ i3-msg workspace 2:todo
 i3-msg append_layout ~/projects/config/i3/restore/todoist.json
 $scripts/startChrome.sh --new-window todoist.com&
 
+sleep 1
+
 # Open Gmail + (Thunderbird)
 i3-msg workspace 3:mail
 if [[ $systemName == "ita" ]]; then
@@ -19,6 +21,8 @@ if [[ $systemName == "ita" ]]; then
 fi
 i3-msg append_layout ~/projects/config/i3/restore/gmail.json
 $scripts/startChrome.sh --new-window mail.google.com&
+
+sleep 1
 
 # Spotify wont listen anyways because its obviously a garbage program written by garbage people so just go to the workspace and open it
 i3-msg workspace 1:music
