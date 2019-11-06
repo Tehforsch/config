7576f205d9f6fbc40e53921833b04710dc7b9377
Tehforsch <Tonipeter92@googlemail.com>
2019-11-06 15:33:15 +0100

    remove some lines which removed files after parsing latex which made it unnecessarily slow

--- scripts/parseLatex.sh
+++ scripts/parseLatex.sh
@@ -7,12 +7,3 @@ else
 fi
 withoutExtension=${latexFileName/.tex/}
 rubber -d -s $latexFileName
-rm ${withoutExtension}-blx.bib
-rm ${withoutExtension}.aux
-rm ${withoutExtension}.bbl
-rm ${withoutExtension}.blg
-rm ${withoutExtension}.dvi
-rm ${withoutExtension}.obj
-rm ${withoutExtension}.out
-rm ${withoutExtension}.tdo
-rm ${withoutExtension}.run.xml
