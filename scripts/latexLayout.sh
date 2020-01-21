3b05c08a81454d045aebcb018d6a2e2d9597f599
Tehforsch <Tonipeter92@googlemail.com>
2020-01-21 10:15:52 +0100

    update scripts

--- scripts/latexLayout.sh
+++ scripts/latexLayout.sh
@@ -9,9 +9,9 @@ fi
 touch $latexFileName
 withoutExtension=${latexFileName/.tex/}
 i3-msg append_layout ~/projects/config/i3/restore/latex.json
-~/.bin/terminal --working-directory=$(pwd) -x "~/.trigger/trigger '~/projects/config/scripts/parseLatex.sh $latexFileName' $(fd .\*.tex); zsh"
+~/.bin/terminal --working-directory=$(pwd) -x "sleep 0.8; ~/.trigger/trigger '~/projects/config/scripts/parseLatex.sh $latexFileName' $(fd .\*.tex); zsh"
 ~/.bin/terminal --working-directory=$(pwd)
 okular ${withoutExtension}.pdf&
-sleep 0.1
+sleep 0.3
 python3 ~/projects/config/i3Contexts/switch.py workspace y --move
 nvim $latexFileName
--- scripts/mountClusterSshfs.sh
+++ scripts/mountClusterSshfs.sh
@@ -1,2 +1,4 @@
-mkdir -p ~/projects/cluster
-sshfs -odebug hd_hp240@bwprod:/beegfs/home/hd/hd_hd/hd_hp240/projects ~/projects/cluster
+mkdir -p ~/projects/cluster/home
+sshfs -odebug hd_hp240@bwprod:/beegfs/home/hd/hd_hd/hd_hp240/projects ~/projects/cluster/home &
+mkdir -p ~/projects/cluster/workspace
+sshfs -odebug hd_hp240@bwprod:/beegfs/work/ws/hd_hp240-arepoTest-0 ~/projects/cluster/workspace
