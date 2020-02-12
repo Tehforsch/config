8cf96b7bb17511ccd5af3ea36272109c728b26fa
Tehforsch <Tonipeter92@googlemail.com>
2020-02-12 15:46:00 +0100

    fix startup script

--- startupScripts/openProgramsAtStartOfSession.sh
+++ startupScripts/openProgramsAtStartOfSession.sh
@@ -2,38 +2,38 @@
 source ~/.localConfig
 
 # TELEGRAM
-python3 ~/projects/config/i3Contexts/switch.py workspace chat
+i3-msg workspace 7:chat
 i3-msg append_layout ~/projects/config/i3/restore/telegram.json
 ~/.apps/Telegram/Telegram&
-sleep 0.3
+sleep 0.8
 
 # TODOIST
-python3 ~/projects/config/i3Contexts/switch.py workspace todo
+i3-msg workspace 8:todo
 i3-msg append_layout ~/projects/config/i3/restore/todoist.json
 $scripts/startChrome.sh --new-window todoist.com&
-sleep 0.8
+sleep 2.0
 
 # THUNDERBIRD
 if [[ $systemName == "ita" ]]; then
-    python3 ~/projects/config/i3Contexts/switch.py workspace mail
+    i3-msg workspace 9:mail
     i3-msg append_layout ~/projects/config/i3/restore/thunderbird.json
     thunderbird&
     sleep 0.8
 fi
 
 # GMAIL
-python3 ~/projects/config/i3Contexts/switch.py workspace gmail
+i3-msg workspace 10:gmail
 i3-msg append_layout ~/projects/config/i3/restore/gmail.json
 $scripts/startChrome.sh --new-window mail.google.com&
 sleep 0.8
 
 # ZOTERO
-python3 ~/projects/config/i3Contexts/switch.py workspace zotero
+i3-msg workspace 11:zotero
 i3-msg append_layout ~/projects/config/i3/restore/zotero.json
 ~/.apps/Zotero_linux-x86_64/zotero &
 sleep 1
 
 # Music
-python3 ~/projects/config/i3Contexts/switch.py workspace music
-sleep 0.2
+i3-msg workspace 6:music
+sleep 0.8
 ~/.bin/terminal --working-directory=$(pwd) -x "ncmpcpp; zsh"&
