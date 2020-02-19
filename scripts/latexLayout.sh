ba2707f22a757c19b94199987445752ecea6e9b7
Tehforsch <Tonipeter92@googlemail.com>
2020-02-19 14:31:54 +0100

    update latex layout

--- scripts/latexLayout.sh
+++ scripts/latexLayout.sh
@@ -9,9 +9,11 @@ fi
 touch $latexFileName
 withoutExtension=${latexFileName/.tex/}
 i3-msg append_layout ~/projects/config/i3/restore/latex.json
-~/.bin/terminal --working-directory=$(pwd) -x "sleep 0.8; ~/.trigger/trigger '~/projects/config/scripts/parseLatex.sh $latexFileName' $(fd .\*.tex); zsh"
+~/.bin/terminal --working-directory=$(pwd) -- zsh -c "sleep 0.8; ~/.trigger/trigger '~/projects/config/scripts/parseLatex.sh $latexFileName' $(fd .\*.tex); zsh -i"
 ~/.bin/terminal --working-directory=$(pwd)
-okular ${withoutExtension}.pdf&
+emacs $latexFileName&
+
 sleep 0.3
-python3 ~/projects/config/i3Contexts/switch.py workspace y --move
-nvim $latexFileName
+i3-msg workspace 13:y
+
+okular ${withoutExtension}.pdf&
