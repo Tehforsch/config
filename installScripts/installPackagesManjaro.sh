7a4c7ed42d999de3d9b5ea72db1385570cd98ef3
Tehforsch <Tonipeter92@googlemail.com>
2020-03-12 13:36:36 +0100

    add background color

--- installScripts/installPackagesManjaro.sh
+++ installScripts/installPackagesManjaro.sh
@@ -1,1 +1,1 @@
-pacman -S firefox python3 community/neovim extra/python-pip community/fd community/i3blocks community/the_silver_searcher
+pacman -S firefox python3 community/neovim extra/python-pip community/fd community/i3blocks community/the_silver_searcher extra/xorg-xsetroot
