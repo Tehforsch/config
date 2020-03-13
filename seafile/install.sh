bf01b336c76360e0ee291f23a9d6196b377ad13c
Tehforsch <Tonipeter92@googlemail.com>
2020-03-13 13:06:35 +0100

    update seafile install script to maybe even work next time.

--- seafile/install.sh
+++ seafile/install.sh
@@ -1,6 +1,6 @@
 # I have no idea why this is not even remotely in the package manager. Its super annoying. Anyways this is basically
 # https://github.com/haiwen/libsearpc/blob/master/INSTALL
-pamac build seafile-client
+pamac build seafile
 mkdir ~/.seafileClient
 cd ~/.seafileClient
 git clone https://github.com/haiwen/seafile-client
@@ -8,3 +8,6 @@ cd seafile-client
 cmake -DBUILD_SHIBBOLETH_SUPPORT=on
 make
 sudo make install
+
+
+echo "To set up: Use the server: https://heibox.uni-heidelberg.de\nwith single sign on!"
