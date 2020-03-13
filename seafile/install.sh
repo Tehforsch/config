c952179b336432c757dedea59aa43cb5744e87f0
Tehforsch <Tonipeter92@googlemail.com>
2020-03-13 12:29:08 +0100

    i hate installing seafile in manjaro.

--- seafile/install.sh
+++ seafile/install.sh
@@ -1,3 +1,6 @@
+# I have no idea why this is not even remotely in the package manager. Its super annoying. Anyways this is basically
+# https://github.com/haiwen/libsearpc/blob/master/INSTALL
+pamac build seafile-client
 mkdir ~/.seafileClient
 cd ~/.seafileClient
 git clone https://github.com/haiwen/seafile-client
