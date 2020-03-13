18b7f8bf9cb3e790b29c9695b64f5e627e5def82
Tehforsch <Tonipeter92@googlemail.com>
2020-03-13 02:31:57 +0100

    add a pre-commit hook for this repo which checks if the list of installed packages is up to date.

---
+++ installScripts/comparePackages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+LIST_PACKAGES="pacman -Qtqn"
+installedPackages=$($LIST_PACKAGES)
+packagesInRepo=$(cat $SCRIPTPATH/packageList)
+isDifferent=$(cmp <(echo $installedPackages | tr " " "\n" | sort) <(echo $packagesInRepo | tr " " "\n" | sort))
+if [[ $isDifferent ]]; then
+    echo "There are packages installed that are not in the package list or vice versa. Consider adding them to the package list (use pacman -Qtqn to generate package list)!"
+    echo $(diff <(echo $installedPackages | tr " " "\n" | sort) <(echo $packagesInRepo | tr " " "\n" | sort))
+else
+    echo "All packages are up to date."
+fi
