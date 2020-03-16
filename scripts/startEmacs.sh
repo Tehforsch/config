1b24928c669b62f2232786fa19d41f59614b7afb
Tehforsch <Tonipeter92@googlemail.com>
2020-03-16 21:26:41 +0100

    remove now unnecessary difference between systems

--- scripts/startEmacs.sh
+++ scripts/startEmacs.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-if [[ $systemName == "ita" ]]; then
-    EMACS_STANDALONE="/usr/bin/emacs26"
-    EMACS_CLIENT="/usr/bin/emacsclient26"
-else
-    EMACS_STANDALONE="emacs"
-    EMACS_CLIENT="emacsclient"
-fi
+EMACS_STANDALONE="emacs"
+EMACS_CLIENT="emacsclient"
 if [[ $EMACS_TEST == 1 ]]; then
     eval $EMACS_STANDALONE
 else
