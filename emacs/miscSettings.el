ff2a4148fb52f3eebaade4fc4369476076ec7f0f
Tehforsch <Tonipeter92@googlemail.com>
2020-04-06 18:38:27 +0200

    Close help buffer with escape and focus them automatically when opening.

--- emacs/miscSettings.el
+++ emacs/miscSettings.el
@@ -3,3 +3,9 @@
 
 ; Don't loop over buffers that are not associated with a file
 (set-frame-parameter nil 'buffer-predicate #'buffer-file-name)
+
+; Focus help buffer automatically when using help functions and allow escaping them with escape
+;; (evil-set-initial-state 'help-mode 'motion)
+(setq help-window-select t)
+(add-hook 'help-mode-hook
+    (lambda () (evil-define-key 'normal help-mode-map [escape] 'kill-buffer-and-window)))
