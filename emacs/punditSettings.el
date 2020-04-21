9ba5f01b3baa322b9327de3d8868fa3163b287dd
Tehforsch <Tonipeter92@googlemail.com>
2020-04-21 13:55:48 +0200

    cleanup .emacs

--- emacs/punditSettings.el
+++ emacs/punditSettings.el
@@ -4,3 +4,7 @@
 (setq default-link-filter-predicate (pundit--has-no-link-to-note-predicate (pundit--get-note-from-title pundit-journal-note-name)))
 (defcustom pundit-date-title-format "%Y %m %d"
   "Format string passed to `format-time-string' for getting a date file's title.")
+
+; Run on startup
+(if (not (boundp 'stored-note-links))
+    (pundit--read-in-note-files))
