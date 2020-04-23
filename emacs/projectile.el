ada6eb2ba7b2aa3cdf4ab4d50e64fda226d2144a
Tehforsch <Tonipeter92@googlemail.com>
2020-04-24 00:04:07 +0200

    refactor

--- emacs/projectile.el
+++ emacs/projectile.el
@@ -1,8 +1,7 @@
 (use-package projectile
     :config
     (setq projectile-mode 1)
-    (setq projectile-project-search-path '("~/projects/"))
-)
+    (setq projectile-project-search-path '("~/projects/")))
 
 (projectile-mode)
 (setq projectile-project-search-path '("~/projects/"))
@@ -14,8 +13,8 @@
         (helm-build-dummy-source
             "Create file"
           :action 'find-file))
-    (add-to-list 'helm-projectile-sources-list helm-source-file-not-found t)
-)
+    (add-to-list 'helm-projectile-sources-list helm-source-file-not-found t))
+
 (setq projectile-switch-project-action 'helm-projectile)
 (use-package helm-etags-plus)
 
@@ -24,13 +23,11 @@
     (interactive)
     (if (projectile-project-p)
         (helm-projectile-find-file)
-        (helm-find-files ".")
-    )
-)
+        (helm-find-files ".")))
 
-; Create new file if helm query for files in the project doesnt result in a file.
-(defvar helm-source-file-not-found (helm-build-dummy-source "Create file" :action 'find-file))
-(add-to-list 'helm-projectile-sources-list helm-source-file-not-found t)
+;; ; Create new file if helm query for files in the project doesnt result in a file.
+;; (defvar helm-source-file-not-found (helm-build-dummy-source "Create file" :action 'find-file))
+;; (add-to-list 'helm-projectile-sources-list helm-source-file-not-found t)
 
 ; Make the name of the file in the status bar relative to projectile path
 (defun my-proj-relative-buf-name ()
@@ -39,5 +36,3 @@
      (file-relative-name buffer-file-name (projectile-project-root)))))
 
 (add-hook 'find-file-hook #'my-proj-relative-buf-name)
-
-(setq projectile-projects-cache (make-hash-table))
