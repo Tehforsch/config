46218883dd4a18daa568cb7fcc8e2ad1fa9756f2
Tehforsch <Tonipeter92@googlemail.com>
2020-04-24 00:16:46 +0200

    make default bookmark names more meaningful

--- emacs/bookmarks.el
+++ emacs/bookmarks.el
@@ -2,4 +2,7 @@
 
 (defun bookmark-set-with-default-name ()
   (interactive)
-  (bookmark-set (file-name-nondirectory buffer-file-name)))
+  (let ((filename (file-name-nondirectory buffer-file-name))
+        (line-number (number-to-string (line-number-at-pos)))
+        (function-name (which-function)))
+    (bookmark-set (concat  filename ":" line-number ":" function-name))))
