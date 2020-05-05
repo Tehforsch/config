99b558e42d256a72159148c092c1e21edf1c71fd
Tehforsch <Tonipeter92@googlemail.com>
2020-05-05 14:29:10 +0200

    actually play something and catch empty play requests

--- scripts/musicSelection/playSong.sh
+++ scripts/musicSelection/playSong.sh
@@ -7,17 +7,26 @@ formatString="%track% - %title%"
 artist=$1
 album=$2
 title=$3
+if [[ $artist == "" ]]; then # Aborted query
+   exit 1
+fi
+if [[ $album == "" ]]; then # Aborted query
+   exit 1
+fi
+if [[ $title == "" ]]; then # Aborted query
+   exit 1
+fi
 echo "Playing:"
 echo "$artist"
 echo "$album"
 echo "$title"
 # I don't know that there is a way to jump to a song directly, while still adding the entire album to the queue.
 # Therefore we will now hack our way around this by playing the album and then going to the next track exactly as many times as needed
-# mpc clear
-# mpc findadd album "$album"
-# mpc play
-# # Find track number
-# trackNumber=$(mpc list track album "$album" title "$title")
-# for i in $(seq 2 $trackNumber); do
-#     mpc next
-# done
+mpc clear
+mpc findadd album "$album"
+mpc play
+# Find track number
+trackNumber=$(mpc list track album "$album" title "$title")
+for i in $(seq 2 $trackNumber); do
+    mpc next
+done
