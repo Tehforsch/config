60551aca5301aa0096f38eb52e2b3ac10d842211
Tehforsch <Tonipeter92@googlemail.com>
2020-05-14 23:49:46 +0200

    add build only option to runrust and make it not show warnings if there are errors (unfortunately it has to recompile for this)

--- scripts/runRust.sh
+++ scripts/runRust.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 args="$@"
-command="cargo clippy; cargo run -- $args"
+if [ "$1" = "build" ]; then
+    command="cargo rustc -- -Awarnings && cargo clippy"
+else
+    command="cargo rustc -- -Awarnings && cargo clippy && cargo run -- $args"
+fi
 trigger "$command" $(fd ".*\.rs$")
