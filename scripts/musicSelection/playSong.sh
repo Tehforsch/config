03ec9b4cd20018c5a8155432f8cf2d45540f94ed
Tehforsch <Tonipeter92@googlemail.com>
2020-05-17 11:15:03 +0200

    fix for albums with many discs

--- scripts/musicSelection/playSong.sh
+++ scripts/musicSelection/playSong.sh
@@ -25,8 +25,19 @@ echo "$title"
 mpc clear
 mpc findadd album "$album"
 mpc play
-# Find track number
+discNumber=$(mpc list disc album "$album" title "$title")
 trackNumber=$(mpc list track album "$album" title "$title")
+# I thought I could implement this with a while loop that queries for the currently playing title with mpc current but then there will be playback of small excerpts of all songs between the first and the one we want which is really annoying so instead we do some math:
+math=$(expr $discNumber - 1)
+for i in $(seq 1 $(expr $discNumber - 1)); do
+    numTracksOnThisAlbum=$(mpc list title disc "$i" album "$album" artist "$artist" | wc -l)
+    for i in $(seq 1 $numTracksOnThisAlbum); do
+        mpc next
+        echo "Skip"
+    done
+done
+# We are now on the correct disc.
 for i in $(seq 2 $trackNumber); do
     mpc next
+    echo "Skip2"
 done
