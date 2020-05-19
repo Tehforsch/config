351d18d292acf0bc940e451e62805d95900cf234
Tehforsch <Tonipeter92@googlemail.com>
2020-05-19 12:05:47 +0200

    add filetype prompts with rofi that open the respective file viewer

---
+++ scripts/rofiFile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd ~
+docs=""
+command=$1
+shift # Filetypes are now the only remaining arguments
+# Put the lower-depth results on top of the list
+maxDepth=3 # we cant do this for all the depths because the query will take too long for it to be fun
+filetypeCommand=""
+for filetype in "$@"; do
+    filetypeCommand=$filetypeCommand" -e $filetype"
+done
+
+for depth in $(seq 0 $maxDepth); do
+    searchResult=$(fd $filetypeCommand --exact-depth $depth)
+    if [ "$searchResult" != "" ]; then
+        if [ "$docs" != "" ]; then
+            docs="${docs}\n$searchResult"
+        else
+            docs="$searchResult"
+        fi
+    fi
+done
+docs=$docs$(fd $filetypeCommand --min-depth $(( $maxDepth + 1 )))
+
+file=$(printf "$docs" | rofi -dmenu -p  "Documents:" -no-custom)
+if [ "$file" != "" ]; then
+    $command "$file"
+fi
