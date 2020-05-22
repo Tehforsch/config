02ba1c9e59e4d4943ca9064655e14bff88a0d030
Tehforsch <Tonipeter92@googlemail.com>
2020-05-22 17:18:38 +0200

    add some lsp settings

--- emacs/lsp.el
+++ emacs/lsp.el
@@ -1,2 +1,12 @@
 (use-package lsp-mode)
 (setq lsp-enable-symbol-highlighting nil)
+
+(use-package lsp-ui)
+
+(use-package ccls
+  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+         (lambda () (require 'ccls) (lsp))))
+
+(add-hook 'c-mode-hook lsp-ui-mode)
+(add-hook 'c-mode-hook '(lambda (lsp-ui-doc-mode nil)))
+(add-hook 'rustic-mode-hook lsp-ui-mode)
