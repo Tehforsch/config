f22d7e6275ac3ace5439b100d778fe3c2b7bfce3
Tehforsch <Tonipeter92@googlemail.com>
2020-05-25 19:36:12 +0200

    update music scripts

--- scripts/musicSelection/newSongThisAlbum.sh
+++ scripts/musicSelection/newSongThisAlbum.sh
@@ -4,4 +4,8 @@ artist=$(printf "$currentAlbumArtist" | cut -f 1)
 album=$(printf "$currentAlbumArtist" | cut -f 2)
 songs=$(mpc find artist "$artist" album "$album")
 path=$(dirname $(realpath $0))
+trackNum=
+
+discNumber=$(mpc list disc album "$album" title "$title")
+trackNumber=$(mpc list track album "$album" title "$title")
 $path/songSelection.sh "$artist" "$album"
