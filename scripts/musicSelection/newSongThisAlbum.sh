45b3bcd98dc7c57056e97752e4590b9cacc9bd4a
Tehforsch <Tonipeter92@googlemail.com>
2020-05-25 10:43:29 +0200

    add script to choose new song from current album

---
+++ scripts/musicSelection/newSongThisAlbum.sh
@@ -0,0 +1,7 @@
+currentAlbumArtist=$(mpc -f "%artist%\t%album%" | head -n 1)
+echo $currentAlbumArtist
+artist=$(printf "$currentAlbumArtist" | cut -f 1)
+album=$(printf "$currentAlbumArtist" | cut -f 2)
+songs=$(mpc find artist "$artist" album "$album")
+path=$(dirname $(realpath $0))
+$path/songSelection.sh "$artist" "$album"
