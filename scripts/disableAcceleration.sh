b96e747dc15598cf29c7b814910ff313953ecbd4
Tehforsch <Tonipeter92@googlemail.com>
2020-05-25 10:44:10 +0200

    loop over more indices to find the correct xinput id

--- scripts/disableAcceleration.sh
+++ scripts/disableAcceleration.sh
@@ -1,4 +1,4 @@
 # Disable mouse acceleration, should be some id between 10-12 
-xinput set-prop 10 "libinput Accel Speed" -1.0
-xinput set-prop 11 "libinput Accel Speed" -1.0
-xinput set-prop 12 "libinput Accel Speed" -1.0
+for id in $(seq 1 15); do
+    xinput set-prop $id "libinput Accel Speed" -1.0
+done
