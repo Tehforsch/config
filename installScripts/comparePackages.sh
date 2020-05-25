9767a77529647fb917b539591f883372bf70624b
Tehforsch <Tonipeter92@googlemail.com>
2020-05-25 19:35:29 +0200

    update package list, remove python modules because there is no way that will ever not be annoying

--- installScripts/comparePackages.sh
+++ installScripts/comparePackages.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 LIST_PACKAGES="pacman -Qeqn"
-installedPackages=$($LIST_PACKAGES)
+installedPackages=$($LIST_PACKAGES | sort)
 systemName=$(cat ~/.systemName)
 localPackagesInRepo=$(cat $SCRIPTPATH/${systemName}PackageList)
-packagesInRepo=$(cat $SCRIPTPATH/packageList)
-isDifferent=$(cmp <(echo $installedPackages | tr " " "\n" | sort) <(echo $packagesInRepo $localPackagesInRepo | tr " " "\n" | sort))
+globalPackagesInRepo=$(cat $SCRIPTPATH/packageList)
+packagesInRepo=$(printf "$localPackagesInRepo\n$globalPackagesInRepo" | sort)
+isDifferent=$(cmp <(echo "$installedPackages") <(echo "$packagesInRepo"))
 if [[ $isDifferent ]]; then
-    echo "There are packages installed that are not in the package list or vice versa. Consider adding them to the package list (use pacman -Qeqn to generate package list)!"
-    echo $(diff <(echo $installedPackages | tr " " "\n" | sort) <(echo $packagesInRepo $localPackagesInRepo | tr " " "\n" | sort)) | tr "<" "\n" | tr ">" "\n"
+    echo "There are packages installed that are not in the package list or vice versa. Consider adding them to the package list (use listPackages.sh to generate package list)!"
+    echo "Packages in repo that are not yet installed:"
+    difference=$(diff <(echo "$installedPackages") <(echo "$packagesInRepo"))
+    echo "$difference" | grep ">" | tr ">" " "
+    echo "Packages that are installed but not in repo:"
+    echo "$difference" | grep "<" | tr "<" " "
+    # echo $(diff <(echo $installedPackages | tr " " "\n" | sort) <(echo $packagesInRepo $localPackagesInRepo | tr " " "\n" | sort)) | tr "<" "<\n" | tr ">" ">\n"
 else
     echo "All packages are up to date."
 fi
