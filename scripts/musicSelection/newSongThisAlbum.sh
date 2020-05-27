8102ff1c7c00e07be236e7d15eba159dad7aa63d
Tehforsch <Tonipeter92@googlemail.com>
2020-05-27 21:12:07 +0200

    preselect currently playing song

--- scripts/musicSelection/newSongThisAlbum.sh
+++ scripts/musicSelection/newSongThisAlbum.sh
@@ -1,11 +1,11 @@
-currentAlbumArtist=$(mpc -f "%artist%\t%album%" | head -n 1)
-echo $currentAlbumArtist
-artist=$(printf "$currentAlbumArtist" | cut -f 1)
-album=$(printf "$currentAlbumArtist" | cut -f 2)
+currentAlbumArtistTitle=$(mpc -f "%artist%\t%album%\t%title%" | head -n 1)
+artist=$(printf "$currentAlbumArtistTitle" | cut -f 1)
+album=$(printf "$currentAlbumArtistTitle" | cut -f 2)
+title=$(printf "$currentAlbumArtistTitle" | cut -f 3)
 songs=$(mpc find artist "$artist" album "$album")
-path=$(dirname $(realpath $0))
-trackNum=
 
-discNumber=$(mpc list disc album "$album" title "$title")
-trackNumber=$(mpc list track album "$album" title "$title")
-$path/songSelection.sh "$artist" "$album"
+numInPlaylist=$(mpc playlist -f "%title%" | grep -n "^$title\$" | cut -d ":" -f 1)
+indexInPlaylist=$(( numInPlaylist - 1 ))
+echo $indexInPlaylist
+path=$(dirname $(realpath $0))
+$path/songSelection.sh --preselect $indexInPlaylist "$artist" "$album"
--- scripts/musicSelection/songSelection.sh
+++ scripts/musicSelection/songSelection.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Select song from a specific album and a specific artist. This is always called
 # after any album selection
+if [ $1 == "--preselect" ]; then
+    songIndex=$2
+    shift; shift
+else
+    songIndex=1
+fi
+
 if [[ $# -ne 2 ]]; then
     exit 1
 fi
@@ -8,7 +15,7 @@ formatString="%title%"
 artist=$1
 album=$2
 titles=$(mpc -f "$formatString" find album "$album" artist "$artist")
-title=$(echo "$titles" | rofi -i -dmenu -no-custom -p "Choose a song:")
+title=$(echo "$titles" | rofi -i -dmenu -no-custom -p "Choose a song:" -selected-row $songIndex)
 if [[ $album == "" ]]; then # Aborted query
    exit 1
 fi
