b772042e4647fa0e76127a9f045e9ad12c1742f4
Tehforsch <Tonipeter92@googlemail.com>
2020-05-27 19:24:21 +0200

    add stream

--- scripts/streamlink.sh
+++ scripts/streamlink.sh
@@ -1,2 +1,2 @@
-stream=$(echo "cr1tdota|arteezy|gorgc|weplayesport_en|beyondthesummit" | rofi -sep "|" -dmenu -p "Choose stream:")
+stream=$(echo "esl_dota2|cr1tdota|arteezy|gorgc|weplayesport_en|beyondthesummit" | rofi -sep "|" -dmenu -p "Choose stream:")
 streamlink twitch.tv/$stream best
