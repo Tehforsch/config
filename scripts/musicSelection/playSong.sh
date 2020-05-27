459246aad296c8788797f7450645b2d16252cc35
Tehforsch <Tonipeter92@googlemail.com>
2020-05-27 19:39:00 +0200

    fix music selection script when many songs match the song title

--- scripts/musicSelection/playSong.sh
+++ scripts/musicSelection/playSong.sh
@@ -20,11 +20,9 @@ echo "Playing:"
 echo "$artist"
 echo "$album"
 echo "$title"
-# I don't know that there is a way to jump to a song directly, while still adding the entire album to the queue.
-# Therefore we will now hack our way around this by playing the album and then going to the next track exactly as many times as needed
-discNumber=$(mpc list disc album "$album" title "$title")
-trackNumber=$(mpc list track album "$album" title "$title")
+
 mpc clear
 mpc findadd album "$album"
-numInPlaylist=$(mpc playlist -f "%title%" | grep -n "$title" | cut -d ":" -f 1)
+numInPlaylist=$(mpc playlist -f "%title%" | grep -n "^$title\$" | cut -d ":" -f 1)
+echo $numInPlaylist
 mpc play $numInPlaylist
