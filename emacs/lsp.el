6101e36657e24a1a60b486e16874cd4032b30566
Tehforsch <Tonipeter92@googlemail.com>
2020-05-27 19:24:13 +0200

    disable lsp for C because somethings not right

--- emacs/lsp.el
+++ emacs/lsp.el
@@ -3,10 +3,10 @@
 
 (use-package lsp-ui)
 
-(use-package ccls
-  :hook ((c-mode c++-mode objc-mode cuda-mode) .
-         (lambda () (require 'ccls) (lsp))))
+;; (use-package ccls
+;;   :hook ((c-mode c++-mode objc-mode cuda-mode) .
+;;          (lambda () (require 'ccls) (lsp))))
 
-(add-hook 'c-mode-hook lsp-ui-mode)
-(add-hook 'c-mode-hook '(lambda (lsp-ui-doc-mode nil)))
-(add-hook 'rustic-mode-hook lsp-ui-mode)
+;; (add-hook 'c-mode-hook lsp-ui-mode)
+;; ;; (add-hook 'c-mode-hook '(lambda () '(lsp-ui-doc-mode nil)))
+;; (add-hook 'rustic-mode-hook lsp-ui-mode)
