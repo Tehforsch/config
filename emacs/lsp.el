0cb3410f30188f259541d0e9e4ef125d2d1a4cc4
Tehforsch <Tonipeter92@googlemail.com>
2020-06-08 19:25:39 +0200

    add pyls to lsp-mode. the main problem was having the installation of pyls from pip which didnt work but the one from manjaro worked

--- emacs/lsp.el
+++ emacs/lsp.el
@@ -1,4 +1,8 @@
-(use-package lsp-mode)
+(use-package lsp-mode
+    :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+            (python-mode . lsp)
+            ;; if you want which-key integration
+            (lsp-mode . lsp-enable-which-key-integration)))
 (setq lsp-enable-symbol-highlighting nil)
 
 ;; (use-package lsp-ui)
