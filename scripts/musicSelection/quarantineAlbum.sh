65d5332f575e2363d6443e36a8d52477d192f985
Tehforsch <Tonipeter92@googlemail.com>
2020-06-16 00:55:03 +0200

    add nicer quarantine selection scripts

--- scripts/musicSelection/quarantineAlbum.sh
+++ scripts/musicSelection/quarantineAlbum.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+
 quarantinedAlbums=$(cat ~/music/quarantine)
 outputSeparator="\t"
-lineNumber=$(echo -E "$quarantinedAlbums" | sed "s/^\"\(.*\)\", \"\(.*\)\"/\1$outputSeparator\2/"  | column -o "   " -s "$(printf "\t")" -t | shuf | rofi -i -dmenu -no-custom -format "d" -p "Album:")
+if [[ $# == 1 ]]; then
+    numAlbums=$(echo -E "$quarantinedAlbums" | wc -l)
+    lineNumber=$(shuf -i 1-$numAlbums -n 1)
+else
+    lineNumber=$(echo -E "$quarantinedAlbums" | sed "s/^\"\(.*\)\", \"\(.*\)\"/\1$outputSeparator\2/"  | column -o "   " -s "$(printf "\t")" -t | shuf | rofi -i -dmenu -no-custom -format "d" -p "Album:")
+fi
 if [[ "$lineNumber" == "" ]]; then
     exit 0
 fi
 line=$(echo -E "$quarantinedAlbums" | head -n $lineNumber | tail -n 1)
-# line2=$(echo "$line" | grep -o "\"(.*)\", \"(.*)\"")
 artist=$(echo "$line" | sed "s/^\"\(.*\)\",.*/\1/")
 album=$(echo "$line" | sed "s/.*, \"\(.*\)\"$/\1/")
-echo $artist
-echo $album
+mpc clear
+mpc findadd album "$album" artist "$artist"
+mpc play
