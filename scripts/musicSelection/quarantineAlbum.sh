cfb6d8667027d8cd39cececf70e62daba66e51ae
Tehforsch <Tonipeter92@googlemail.com>
2020-06-16 10:23:40 +0200

    fix bug

--- scripts/musicSelection/quarantineAlbum.sh
+++ scripts/musicSelection/quarantineAlbum.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-quarantinedAlbums=$(cat ~/music/quarantine)
+quarantinedAlbums=$(cat ~/music/quarantine | shuf)
 outputSeparator="\t"
 if [[ $# == 1 ]]; then
     numAlbums=$(echo -E "$quarantinedAlbums" | wc -l)
     lineNumber=$(shuf -i 1-$numAlbums -n 1)
 else
-    lineNumber=$(echo -E "$quarantinedAlbums" | sed "s/^\"\(.*\)\", \"\(.*\)\"/\1$outputSeparator\2/"  | column -o "   " -s "$(printf "\t")" -t | shuf | rofi -i -dmenu -no-custom -format "d" -p "Album:")
+    lineNumber=$(echo -E "$quarantinedAlbums" | sed "s/^\"\(.*\)\", \"\(.*\)\"/\1$outputSeparator\2/"  | column -o "   " -s "$(printf "\t")" -t | rofi -i -dmenu -no-custom -format "d" -p "Album:")
 fi
+echo $lineNumber
 if [[ "$lineNumber" == "" ]]; then
     exit 0
 fi
 line=$(echo -E "$quarantinedAlbums" | head -n $lineNumber | tail -n 1)
 artist=$(echo "$line" | sed "s/^\"\(.*\)\",.*/\1/")
 album=$(echo "$line" | sed "s/.*, \"\(.*\)\"$/\1/")
-mpc clear
-mpc findadd album "$album" albumartist "$artist"
-mpc play
+if [[ $# == 1 ]]; then
+    mpc clear
+    mpc findadd album "$album" albumartist "$artist"
+    mpc play
+else
+    path=$(dirname $(realpath $0))
+    $path/songSelection.sh "$artist" "$album"
+fi
