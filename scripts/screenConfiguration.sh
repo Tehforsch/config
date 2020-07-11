550a442c23dc81f0b81f27e6e84dc9200eebb4ec
Tehforsch <Tonipeter92@googlemail.com>
2020-07-11 12:59:08 +0200

    add screen config for manjaro pc

--- scripts/screenConfiguration.sh
+++ scripts/screenConfiguration.sh
@@ -8,6 +8,10 @@ if [[ $systemName == "manjaro" ]]; then
     main=HDMI1
     second=eDP1
     xrandr --output $main --primary --auto --output $second --auto --left-of $main
+elif [[ $systemName == "manjaroPc" ]]; then
+    main=DP-0
+    second=HDMI-0
+    xrandr --output $main --primary --auto --output $second --auto --left-of $main
 else
     xrandr --output HDMI-0 --primary
 fi
