5f72fc8bd1297a43402734b17aefa3fec9072440
tehforsch <Tonipeter92@googlemail.com>
2020-08-03 23:39:49 +0200

    QOL changes

--- scripts/keyboardConfiguration.sh
+++ scripts/keyboardConfiguration.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 config=/home/toni/projects/config
 setxkbmap -layout de -variant nodeadkeys -option caps:escape
-xmodmapFile=~/.activeXmodmap 
-if [[ -a $xmodmapFile ]]; then
-    active=$(cat $xmodmapFile)
-else
-    active=1
-fi
-echo "Active xmodmap: $active"
-if [[ $1 == "toggle" ]]; then # goto next xmodmap
-    if [[ $active == 1 ]]; then
-        echo 2 > $xmodmapFile
-        active=2
-    else
-        echo 1 > $xmodmapFile
-        active=1
-    fi
-fi
-echo "Active xmodmap: $active"
-if [[ $active == 1 ]]; then
+# xmodmapFile=~/.activeXmodmap 
+# if [[ -a $xmodmapFile ]]; then
+#     active=$(cat $xmodmapFile)
+# else
+#     active=1
+# fi
+# echo "Active xmodmap: $active"
+# if [[ $1 == "toggle" ]]; then # goto next xmodmap
+#     if [[ $active == 1 ]]; then
+#         echo 2 > $xmodmapFile
+#         active=2
+#     else
+#         echo 1 > $xmodmapFile
+#         active=1
+#     fi
+# fi
+# echo "Active xmodmap: $active"
+# if [[ $active == 1 ]]; then
     xmodmap $config/xmodmap/xmodmapNormal
-else
-    xmodmap $config/xmodmap/xmodmapProgramming
-fi
+# else
+    # xmodmap $config/xmodmap/xmodmapProgramming
+# fi
 
 killall xbindkeys
 sleep 0.5
--- scripts/preGameHook.sh
+++ scripts/preGameHook.sh
@@ -1,9 +1,9 @@
 setxkbmap -layout de -variant nodeadkeys
-xmodmap /home/toni/projects/config/xmodmap/XmodmapGames
+xmodmap /home/toni/projects/config/xmodmap/xmodmapGames
 bash /home/toni/projects/config/scripts/disableAcceleration.sh # This should already have run but make sure it is run
 count=$(pgrep -c obs)
 if [[ $count == 0 ]]; then
-    obs --startreplaybuffer --minimize-to-tray &
+    # obs --startreplaybuffer --minimize-to-tray &
 fi
 # Kill obs killscript. see script for details
 for id in $(pgrep -f killObs.sh); do
