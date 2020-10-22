0215c597c7c279e304c176bc058ffd05086cf9e9
tehforsch <Tonipeter92@googlemail.com>
2020-10-22 23:04:11 +0200

    add scripts to switch between corresponding folders in the sim/run dirs

---
+++ scripts/cdSimDirs.sh
@@ -0,0 +1,23 @@
+if [[ $1 == "sim" ]]; then
+    a="run"
+    b="simulations"
+fi
+if [[ $1 == "run" ]]; then
+    a="simulations"
+    b="run"
+fi
+
+pwd=$(pwd)
+if [[ $pwd == *"phd/$a/"* ]]; then
+    newDir=${pwd/$a/$b}
+else
+    newDir=~/projects/phd/$b
+fi
+if [[ $pwd == *"phd/$b/"* ]]; then
+else
+    while ! [[ -d $newDir ]]; do
+        newDir=$(dirname $newDir)
+        echo $newDir
+    done
+    cd $newDir
+fi
