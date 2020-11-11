4ebb91d225fbddf6738dbff946ab9ef963693e95
tehforsch <Tonipeter92@googlemail.com>
2020-11-11 12:10:44 +0100

    clear before running

--- scripts/runRust.sh
+++ scripts/runRust.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 args="$@"
 if [ "$1" = "build" ]; then
-    command="cargo rustc -- -Awarnings && cargo clippy"
+    command="clear; cargo rustc -- -Awarnings && cargo clippy"
 else
-    command="cargo rustc -- -Awarnings && cargo clippy && cargo run -- $args"
+    command="clear; cargo rustc -- -Awarnings && cargo clippy && cargo run -- $args"
 fi
 trigger "$command" $(fd ".*\.rs$")
