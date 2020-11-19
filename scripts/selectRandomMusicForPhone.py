60e8ad7eaca091bd57e0a26c7312149181c93ffb
tehforsch <Tonipeter92@googlemail.com>
2020-11-19 17:31:59 +0100

    add simple script to choose random music to copy onto the phone

---
+++ scripts/selectRandomMusicForPhone.py
@@ -0,0 +1,18 @@
+import shutil
+from pathlib import Path
+import random
+import os
+
+source = "/home/toni/music/"
+target = "/home/toni/musicForPhone/"
+
+albums = [Path(source, x) for x in os.listdir(source) if Path(source, x).is_dir()]
+
+numArtists = 100
+randomArtists = random.sample(albums, numArtists)
+for artist in randomArtists:
+    # ugly as shit but my back hurts and i wanna get this done
+    source = artist
+    target = str(artist).replace("music/", "musicForPhone/")
+    print(source, target)
+    shutil.copytree(source, target)
