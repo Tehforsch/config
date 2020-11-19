e701d06a17bdfe98b46e328135c504e486b1af56
tehforsch <Tonipeter92@googlemail.com>
2020-11-19 17:31:30 +0100

    another attempt at making rustic mode work properly for config files

--- emacs/rust.el
+++ emacs/rust.el
@@ -1,6 +1,8 @@
 (use-package rustic)
 (setq rustic-format-on-save t)
 (add-hook 'rustic-mode-hook 'flycheck-mode)
+; I hate emacs for this. I dont know why but auto-mode-alist seems to be ignored completely and every now and then .rs files will be opened in other garbage modes
 (setq auto-mode-alist (rassq-delete-all 'conf-colon-mode auto-mode-alist))
 (setq auto-mode-alist (rassq-delete-all 'image-mode auto-mode-alist))
-(add-to-list 'auto-mode-alist '("\\.rs$" . rustic-mode))
+(setq auto-mode-alist (rassq-delete-all 'm2-mode auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rustic-mode))
