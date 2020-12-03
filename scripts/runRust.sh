f65a5a4167f35dd5589844e79f74c5d6d68f438b
tehforsch <Tonipeter92@googlemail.com>
2020-12-03 12:34:40 +0100

    allow parameters in commands

--- scripts/runRust.sh
+++ scripts/runRust.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+subcmd="$1"
+shift
 args="$@"
-if [ "$1" = "build" ]; then
-    shift
-    args="$@"
+if [ "$subcmd" = "build" ]; then
     command="clear; cargo rustc $args -- -Awarnings"
-elif [ "$1" = "test" ]; then
-    command="clear; cargo rustc -- -Awarnings && cargo clippy && cargo test -- --nocapture"
+elif [ "$subcmd" = "test" ]; then
+    command="clear; cargo test $args"
+elif [ "$subcmd" = "testVerbose" ]; then
+    command="clear; cargo test -- --nocapture"
 else
     command="clear; cargo rustc -- -Awarnings && cargo clippy && cargo run -- $args"
 fi
