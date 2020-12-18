e95a863b1c9da17f2c2c18167ef64923ed6c2469
tehforsch <Tonipeter92@googlemail.com>
2020-12-18 13:57:12 +0100

    i am an idiot

--- emacs/rust.el
+++ emacs/rust.el
@@ -4,10 +4,6 @@
 
 (push 'rustic-clippy flycheck-checkers)
 (add-hook 'rustic-mode-hook 'flycheck-mode)
-; I hate emacs for this. I dont know why but auto-mode-alist seems to be ignored completely and every now and then .rs files will be opened in other garbage modes
-; I found out that this is due to rustfmt being called by rustic upon save. Afterwards the file is reloaded which triggers the find-file-hook. There, the zil/link-file-open-hook is called which sets the conf mode for some reason. I cant be bothered to debug this properly so I just removed the hook. Now files called config.rs have the right mode.
-(setq find-file-hook (remove 'zil/link-file-open-hook find-file-hook))
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rustic-mode))
 (setq rustic-lsp-server 'rust-analyzer)
 
 (use-package company)
--- emacs/symlinks.el
+++ emacs/symlinks.el
@@ -1,14 +1,2 @@
 ; Always follow symlinks without asking
 (setq vc-follow-symlinks t)
-
-; When opening symlink file: Find filetype from symlink (see https://emacs.stackexchange.com/questions/34756/emacs-linked-to-emacs-link-open-emacs-link-in-emacs-lisp-mode)
-(defun zil/link-file-open-hook ()
-    "for link files' major-mode in dot-file"
-    (let* ((bn (buffer-file-name))
-    (dot-name (concat "." (file-name-base bn)))
-    (mode (assoc-default dot-name auto-mode-alist 'string-match)))
-    (if (and (string= "link" (file-name-extension bn)))
-    (string-match-p "dot" bn))
-    (and mode (set-auto-mode-0 mode t))))
-
-    (add-hook 'find-file-hook 'zil/link-file-open-hook)
