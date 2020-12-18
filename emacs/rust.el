d789dd509bed3ad089e4306eb0fa1e0579a131a0
tehforsch <Tonipeter92@googlemail.com>
2020-12-18 13:46:34 +0100

    finally fix problem with config.rs files being in wrong mode

--- emacs/rust.el
+++ emacs/rust.el
@@ -5,9 +5,8 @@
 (push 'rustic-clippy flycheck-checkers)
 (add-hook 'rustic-mode-hook 'flycheck-mode)
 ; I hate emacs for this. I dont know why but auto-mode-alist seems to be ignored completely and every now and then .rs files will be opened in other garbage modes
-(setq auto-mode-alist (rassq-delete-all 'conf-colon-mode auto-mode-alist))
-(setq auto-mode-alist (rassq-delete-all 'image-mode auto-mode-alist))
-(setq auto-mode-alist (rassq-delete-all 'm2-mode auto-mode-alist))
+; I found out that this is due to rustfmt being called by rustic upon save. Afterwards the file is reloaded which triggers the find-file-hook. There, the zil/link-file-open-hook is called which sets the conf mode for some reason. I cant be bothered to debug this properly so I just removed the hook. Now files called config.rs have the right mode.
+(setq find-file-hook (remove 'zil/link-file-open-hook find-file-hook))
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rustic-mode))
 (setq rustic-lsp-server 'rust-analyzer)
 
@@ -19,3 +18,5 @@
 ;; (require 'rust-mode)
 ;; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 ;; (setq company-tooltip-align-annotations t)
+
+(add-hook 'conf-space-mode-hook 'backtrace)
