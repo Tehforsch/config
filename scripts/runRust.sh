2bac50a94ccb799eeb0ac6f6497b98cb8bec7050
tehforsch <Tonipeter92@googlemail.com>
2020-12-22 12:25:24 +0100

    Update scripts

--- scripts/runRust.sh
+++ scripts/runRust.sh
@@ -5,10 +5,12 @@ args="$@"
 if [ "$subcmd" = "build" ]; then
     command="clear; cargo rustc $args -- -Awarnings"
 elif [ "$subcmd" = "test" ]; then
-    command="clear; cargo rustc $args -- -Awarnings && cargo test $args"
+    command="clear; cargo rustc $args -- -Awarnings && cargo test"
 elif [ "$subcmd" = "testVerbose" ]; then
     command="clear; cargo test -- --nocapture"
+elif [ "$subcmd" = "run" ]; then
+    command="clear&& cargo run $args"
 else
-    command="clear; cargo rustc -- -Awarnings && cargo clippy && cargo run -- $args"
+    echo "subcmd $subcmd not understood"
 fi
 trigger "$command" $(fd ".*\.rs$")
--- scripts/updatePankit.sh
+++ scripts/updatePankit.sh
@@ -1,1 +1,2 @@
+cp --backup=numbered /home/toni/.local/share/Anki2/realUser/collection.anki2 /home/toni/.playground/ankiBU/
 /home/toni/.cargo-target/release/pundit /home/toni/notes pankit /home/toni/.local/share/Anki2/realUser/collection.anki2 ~/.pankitDb 2>&1 | tee -a ~/.pankitOut
