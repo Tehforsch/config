4a810f54729a73be730b5d35c280fa0e8162a387
tehforsch <Tonipeter92@googlemail.com>
2021-01-09 16:50:13 +0100

    improve screen recording script to use xrectsel to specify region and add screen recording mode in i3

--- scripts/screenCapture.sh
+++ scripts/screenCapture.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+sleep 0.1
 if [[ $# == 0 ]]; then
+    geom=$(xrectsel)
+    echo "$geom" >> ~/.playground/sc
+    pat='([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+)'
+    [[ "$geom" =~ $pat ]]
+    xSize="${BASH_REMATCH[1]}"
+    ySize="${BASH_REMATCH[2]}"
+    xOff="${BASH_REMATCH[3]}"
+    yOff="${BASH_REMATCH[4]}"
+    # Make the sizes even which is needed for ffmpeg
+    if [[ $(($xSize % 2)) == 1 ]]; then
+        xSize=$(($xSize + 1))
+    fi
+    if [[ $(($ySize % 2)) == 1 ]]; then
+        ySize=$(($ySize + 1))
+    fi
     mkdir -p ~/.screenCaptures
     outputFile=~/.screenCaptures/$(date +%Y-%m-%d-%H-%M-%S).mkv
-    ffmpeg -f x11grab -video_size 1920x1080 -framerate 30 -i :0.0 -f pulse -i default -preset ultrafast -crf 18 -pix_fmt yuv420p $outputFile
+    ffmpeg -f x11grab -video_size ${xSize}x${ySize} -framerate 30 -i :0.0+${xOff},${yOff} -preset ultrafast -crf 18 -pix_fmt yuv420p $outputFile
 else
     killall ffmpeg
 fi
