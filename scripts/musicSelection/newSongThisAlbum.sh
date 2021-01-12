f70ad0a8c3a733e10aefe841962d75e39e69a68f
tehforsch <Tonipeter92@googlemail.com>
2021-01-12 16:21:37 +0100

    finally fix albumartist problem

--- scripts/musicSelection/newSongThisAlbum.sh
+++ scripts/musicSelection/newSongThisAlbum.sh
@@ -1,8 +1,9 @@
-currentAlbumArtistTitle=$(mpc -f "%artist%\t%album%\t%title%" | head -n 1)
+currentAlbumArtistTitle=$(mpc -f "%albumartist%\t%album%\t%title%" | head -n 1)
 artist=$(printf "$currentAlbumArtistTitle" | cut -f 1)
 album=$(printf "$currentAlbumArtistTitle" | cut -f 2)
 title=$(printf "$currentAlbumArtistTitle" | cut -f 3)
-songs=$(mpc find artist "$artist" album "$album")
+songs=$(mpc find albumartist "$artist" album "$album")
+echo $songs
 
 numInPlaylist=$(mpc playlist -f "%title%" | grep -n "^$title\$" | cut -d ":" -f 1)
 indexInPlaylist=$(( numInPlaylist - 1 ))
