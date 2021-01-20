dc2e7b9aa586969516a81d6dc8c189378edebbcb
tehforsch <Tonipeter92@googlemail.com>
2021-01-20 11:43:26 +0100

    update packages on restart!

--- emacs/python.el
+++ emacs/python.el
@@ -4,6 +4,18 @@
   (insert "import pdb; pdb.set_trace()")
   (evil-normal-state)
 )
+(flycheck-define-checker
+    python-mypy ""
+    :command ("mypy"
+              "--ignore-missing-imports" "--fast-parser"
+              "--python-version" "3.6"
+              source-original)
+    :error-patterns
+    ((error line-start (file-name) ":" line ": error:" (message) line-end))
+    :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-mypy t)
+(flycheck-add-next-checker 'python-pylint 'python-mypy t)
 
 (use-package python-black)
 (add-hook 'python-mode-hook 'python-black-on-save-mode)
