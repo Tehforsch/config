3808ca7e30eebd90fca26c17d4190204e4091802
tehforsch <Tonipeter92@googlemail.com>
2021-02-10 17:37:03 +0100

    add notifications when randoming albums and add functionality to show notification with the current album upon demand (bound to ? in media mode in i3)

--- scripts/musicSelection/randomAlbum.sh
+++ scripts/musicSelection/randomAlbum.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -g|--genre) genre="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
-
-if [[ "$genre" == "" ]]; then
-    album=$(mpc list album | shuf | head -n 1)
-else
-    album=$(mpc list album genre "$genre" | shuf | head -n 1)
-fi
+album=$(mpc list album | shuf | head -n 1)
 mpc clear
 mpc findadd album "$album"
 mpc play
+
+path=$(dirname $(realpath $0))
+$path/showNotification.sh
---
+++ scripts/musicSelection/showNotification.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+message="$(mpc current -f "%artist% - %album% - %title%")"
+# the canonical-private-synchronous info makes sure that this notification replaces itself (so we dont see previous albums anymore when we call this script again)
+notify-send "$message" -h string:x-canonical-private-synchronous:anything -t 1000
