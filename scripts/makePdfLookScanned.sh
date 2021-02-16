aed3ef955eaeb4a3ef5b4d1a92ea0076bec7eb08
tehforsch <Tonipeter92@googlemail.com>
2021-02-16 19:12:35 +0100

    adjust params

--- scripts/makePdfLookScanned.sh
+++ scripts/makePdfLookScanned.sh
@@ -1,4 +1,4 @@
 # Credit to "fortea" from https://tex.stackexchange.com/questions/94523/simulate-a-scanned-paper
 output="scanned.pdf"
 echo "Creating $output"
-convert -density 150 "$1" -linear-stretch 3.5%x10% -blur 0x0.5 -attenuate 0.25 +noise Gaussian "$output"
+convert -density 100 "$1" -linear-stretch 3.5%x10% -blur 0x0.5 -attenuate 0.25 +noise Gaussian "$output"
