8c83732109c7e2a74bb67bb846869cbe1814b27a
tehforsch <Tonipeter92@googlemail.com>
2021-03-04 18:52:26 +0100

    standardize formatting of flycheck buffer

--- emacs/flycheck.el
+++ emacs/flycheck.el
@@ -1,2 +1,18 @@
 (use-package flycheck)
 (setq flycheck-check-syntax-automatically '(mode-enabled save))
+(add-to-list 'display-buffer-alist
+             `(,(rx bos "*Flycheck errors*" eos)
+              (display-buffer-reuse-window
+               display-buffer-in-side-window)
+              (side            . bottom)
+              (reusable-frames . visible)
+              (window-height   . 0.33)))
+
+; (use-package flycheck-projectile)
+
+; (defun helm-flycheck-projectile-errors ()
+;   (let* ((project (projectile-acquire-root nil))
+;                  (list (flycheck-projectile--make-error-list project)))
+;     (message list)))
+
+; (helm-flycheck-projectile-errors)
