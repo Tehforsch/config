c1cc2acc1dc5d34e5243eef1f7b1f7b615616d37
tehforsch <Tonipeter92@googlemail.com>
2021-03-08 10:09:40 +0100

    cleanup

--- dirAwareHistory/history.zsh
+++ dirAwareHistory/history.zsh
@@ -5,16 +5,15 @@ else
 fi
 
 preexec () {
-    # Only write to our own history file if the command does not start with a space (like HIST_IGNORE_SPACE)
+    # Only write to our own history file if the command does not start with a space (like HIST_IGNORE_SPACE). This is mostly so that the ugly commands created by history-aware reverse search can be hidden from history (otherwise we enter a cascade of MEGA-DIRECTORY-AWARE commands)
     initialLetter="$(echo $1 | head -c 1)"
     if ! [[ "$initialLetter" == " " ]]; then
         echo "$(pwd)" "$1"  >> "$HISTORY_FILE" 
     fi
 }
 
-# Alternative way to change history storage if the above doesnt work.
-# This then requires to rewrite all history-related functions
-# such as up-history etc.
+# # Alternative way to change history storage if the above turns out to not work in the long term.
+# # This then requires rewriting of all history-related functions such as up-history etc.
 # function zshaddhistory() {
 #   emulate -L zsh
 #   if ! [[ "$1" =~ "(^ |^ykchalresp|--password)" ]] ; then
