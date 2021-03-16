946f97e895af68e6f939a1425809fd8aecd4c988
tehforsch <Tonipeter92@googlemail.com>
2021-03-16 15:40:15 +0100

    since the i3 script i dont really need inotify any more so i removed save-buffer-always which forces a fresh compile everytime

--- emacs/saveBufferAlways.el
+++
@@ -1,9 +0,0 @@
-; Important for triggering inotify events: save even when not modified
-(defun save-buffer-always ()
-    "Save the buffer even if it is not modified."
-    (interactive)
-    (set-buffer-modified-p t)
-    (save-buffer)
-)
-; Make sure save-buffer-always doesnt ruin . key state from evil. Not sure if this is actually all that important but someone mentioned it so whatever
-(evil-declare-abort-repeat 'save-buffer-always)
