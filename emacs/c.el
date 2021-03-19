17f540eec49e129a72ff16cea842d817d92e4a5a
tehforsch <Tonipeter92@googlemail.com>
2021-03-19 13:49:08 +0100

    finally disable the worlds most annoying feature - hiding ifdefs that arent used (which never works properly)

--- emacs/c.el
+++ emacs/c.el
@@ -1,1 +1,6 @@
 (add-hook 'c-mode-hook 'flycheck-mode)
+(add-hook 'c-mode-hook 'lsp-mode)
+;; (add-hook 'c-mode-hook '(lambda (hide-ifdef-mode nil)))
+;; (add-hook 'c-mode-hook '(lambda (setq default-tab-width 4)))
+
+(setq ccls-enable-skipped-ranges nil)
