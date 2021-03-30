066fe63603ee2e8f0776936718ea7276d6cf2fc4
tehforsch <Tonipeter92@googlemail.com>
2021-03-30 12:04:23 +0200

    fix disable acc script

--- scripts/disableAcceleration.sh
+++ scripts/disableAcceleration.sh
@@ -1,7 +1,6 @@
 # Disable mouse acceleration, should be some id between 10-12 
-for id in $(seq 1 14); do
+for id in $(seq 15 20); do
     xinput set-prop $id "libinput Accel Speed" -0.0
 done
-for id in $(seq 14 20); do
-    xinput set-prop $id "libinput Accel Speed" -1.0
-done
+mouseId=$(xinput list | grep Mouse | cut -f 2 | sed "s/id=//")
+xinput set-prop $mouseId "libinput Accel Speed" -1.0
