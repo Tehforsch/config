95ce166c7d2bca516a053d3990dc053289bafa12
tehforsch <Tonipeter92@googlemail.com>
2021-04-14 10:45:23 +0200

    make timers more useful - beep forever and add a kill hotkey for them

--- scripts/timer/timer.sh
+++ scripts/timer/timer.sh
@@ -1,12 +1,7 @@
 remainTime=300
 timerDir="$( cd "$(dirname "$0")" ; pwd -P )"
-for i in $(seq 1 $1); do
+sleep $1
+for i in $(seq 1 10000); do
+    paplay $timerDir/beep.wav
     sleep 1
 done
-paplay $timerDir/beep.wav
-sleep 1
-paplay $timerDir/beep.wav
-sleep 1
-paplay $timerDir/beep.wav
-sleep 1
-sleep $remainTime
--- scripts/timer/timerAt.sh
+++ scripts/timer/timerAt.sh
@@ -12,15 +12,10 @@ if [[ $# == 1 ]]; then
     # Can't call timer.sh here because that messes up the ps output for some reason
     remainTime=300
     timerDir="$( cd "$(dirname "$0")" ; pwd -P )"
-    for i in $(seq 1 $diff); do
+    sleep $diff
+    for i in $(seq 1 100000); do
+        paplay $timerDir/beep.wav
         sleep 1
     done
-    paplay $timerDir/beep.wav
-    sleep 1
-    paplay $timerDir/beep.wav
-    sleep 1
-    paplay $timerDir/beep.wav
-    sleep 1
-    sleep $remainTime
 fi
 
