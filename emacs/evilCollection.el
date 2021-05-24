b1784b56f5624fb5dd277bfcfda6445643749d5d
tehforsch <Tonipeter92@googlemail.com>
2021-05-24 16:36:56 +0200

    reorder emacs files

--- emacs/evilCollection.el
+++ emacs/evilCollection.el
@@ -1,6 +1,5 @@
 (use-package evil-collection
   :after (evil helm)
   :custom (evil-collection-setup-minibuffer t)
-  :init (evil-collection-init))
-
-(evil-collection-ediff-setup)
+  :init (progn (evil-collection-init)
+               (evil-collection-ediff-setup)))
