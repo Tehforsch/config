315a6818c57800713deae704e2e6ee125a6efd04
tehforsch <Tonipeter92@googlemail.com>
2021-05-24 17:41:33 +0200

    remove unused old pundit

--- emacs/punditSettings.el
+++ emacs/punditSettings.el
@@ -1,24 +1,3 @@
-(setq pundit-directory "~/notes/journal")
-(setq pundit-journal-note-name "journal")
-(setq pundit-paper-note-name "paper")
-(setq default-link-filter-predicate (pundit--has-no-link-to-note-predicate (pundit--get-note-from-title pundit-journal-note-name)))
-(defcustom pundit-date-title-format "%Y %m %d"
-  "Format string passed to `format-time-string' for getting a date file's title.")
-
-(defun pundit-helm-insert-link-to-note-with-custom-title ()
-  (interactive)
-  (pundit-helm-insert-link-to-note nil t))
-
-(defun pundit-helm-append-link-to-note-with-custom-title ()
-  (interactive)
-  (pundit-helm-append-link-to-note nil t))
-
-; Run on startup
-(if (not (boundp 'stored-note-links))
-    (progn
-      (pundit--read-in-note-files)
-      (pundit-validate-all-notes)))
-
 (setq rpundit/directory "~/notes")
 (setq rpundit/anki-collection "~/.local/share/Anki2/User\\ 1/collection.anki2")
 (setq rpundit/executable "~/.cargo/bin/pundit")
