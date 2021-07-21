3e36e9fc9bb05e43e851af5861e33c30a9b14068
tehforsch <mail@tonipeter.de>
2021-07-21 19:09:39 +0200

    fix latex mode -> LaTeX mode which seems to have fixed the annoying paragraphs etc. also remove unused packages

--- emacs/latex.el
+++ emacs/latex.el
@@ -1,21 +1,17 @@
-(use-package auctex
-  :defer t
-  :ensure t)
-
-(use-package cdlatex)
-
+(use-package evil-tex)
 (defun my-reset-paragraph-variables ()
   (interactive)
   (kill-local-variable 'paragraph-start)
   (kill-local-variable 'paragraph-separate))
 
-(add-hook 'latex-mode-hook 'my-reset-paragraph-variables)
 
 (defun set-display-line-numbers ()
   (interactive)
   (setq display-line-numbers t))
 
-(add-hook 'latex-mode-hook 'set-display-line-numbers)
+(add-hook 'LaTeX-mode-hook 'my-reset-paragraph-variables)
+(add-hook 'LaTeX-mode-hook 'set-display-line-numbers)
+(add-hook 'LaTeX-mode-hook #'evil-tex-mode)
 
 (setq latex-bin-folder "/opt/texlive/2021/bin/x86_64-linux/")
 
