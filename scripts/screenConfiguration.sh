9178eb564d9a26fed16ff32085c11ab735245a7f
tehforsch <mail@tonipeter.de>
2021-09-24 10:10:40 +0200

    update screen config for ita monitors

--- scripts/screenConfiguration.sh
+++ scripts/screenConfiguration.sh
@@ -5,13 +5,16 @@ xset s off
 systemName=$(cat ~/.systemName)
 if [[ $systemName == "manjaro" ]]; then
     echo "a"
-    main=HDMI1
+    main=DP2
     second=eDP1
-    xrandr --output $main --primary --auto --output $second --auto --left-of $main
 elif [[ $systemName == "manjaroPc" ]]; then
     main=DP-0
-    second=HDMI-0
-    xrandr --output $main --primary --auto --output $second --auto --right-of $main
+    second=DP2
+elif [[ $systemName == "ita" ]]; then
+    main=DP2
+    second=eDP1
 else
-    xrandr --output HDMI-0 --primary
+    main=DP2
+    second=DP1-3
 fi
+xrandr --output $main --primary --auto --output $second --auto --left-of $main
