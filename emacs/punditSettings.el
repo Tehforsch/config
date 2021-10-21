d437f3407d4ca1ee6502c5125ceee215b1b12dba
tehforsch <mail@tonipeter.de>
2021-10-21 19:52:22 +0200

    restore pundit settings

---
+++ emacs/punditSettings.el
@@ -0,0 +1,5 @@
+(setq rpundit/directory "~/notes")
+(setq rpundit/anki-collection "~/.local/share/Anki2/User\\ 1/collection.anki2")
+(setq rpundit/bibtex-file "~/resource/papers/library.bib")
+(setq rpundit/executable "~/.cargo/bin/pundit")
+(setq rpundit/window-height 30)
