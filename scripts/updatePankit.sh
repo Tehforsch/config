315ee8a741eeb6b0342ef8c17ddc1ecf98e5682a
tehforsch <mail@tonipeter.de>
2021-11-15 20:05:59 +0100

    create anki backup folder if it doesn't exist

--- scripts/updatePankit.sh
+++ scripts/updatePankit.sh
@@ -1,3 +1,4 @@
+mkdir -p ~/.playground/ankiBU
 db="/home/toni/.local/share/Anki2/User 1/collection.anki2"
 cp --backup=numbered "$db" /home/toni/.playground/ankiBU/
 /home/toni/.cargo/bin/pundit /home/toni/notes pankit "$db" ~/.pankitDb 2>&1 | tee -a ~/.pankitOut
