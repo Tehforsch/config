7fb7a9522bdd771b1e02940635afab7f5afa34a2
tehforsch <mail@tonipeter.de>
2021-11-15 20:05:18 +0100

    fix comment

--- scripts/disableAcceleration.sh
+++ scripts/disableAcceleration.sh
@@ -1,6 +1,7 @@
-# Disable mouse acceleration, should be some id between 10-12 
+# Set touchpad acceleration
 for id in $(seq 15 20); do
     xinput set-prop $id "libinput Accel Speed" -0.0
 done
+# Disable for mouse
 mouseId=$(xinput list | grep Mouse | cut -f 2 | sed "s/id=//")
 xinput set-prop $mouseId "libinput Accel Speed" -1.0
