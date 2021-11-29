58983ec5262de04407de53e3542b8b759ac95713
tehforsch <mail@tonipeter.de>
2021-11-29 09:11:42 +0100

    increase mouse speed by a bit, even though this probably makes the accuracy worse :|

--- scripts/disableAcceleration.sh
+++ scripts/disableAcceleration.sh
@@ -2,6 +2,10 @@
 for id in $(seq 15 20); do
     xinput set-prop $id "libinput Accel Speed" -0.0
 done
-# Disable for mouse
+# Disable acceleration for mouse
 mouseId=$(xinput list | grep Mouse | cut -f 2 | sed "s/id=//")
 xinput set-prop $mouseId "libinput Accel Speed" -1.0
+
+# Increase speed slightly
+mouseSpeed=1.15
+xinput set-prop $mouseId "Coordinate Transformation Matrix" $mouseSpeed 0 0 0 $mouseSpeed 0 0 0 1
