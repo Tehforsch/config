f3502db80bbe72d540c56b192569d4cd11178f68
tehforsch <mail@tonipeter.de>
2021-12-13 20:21:17 +0100

    add marginalia

---
+++ emacs/marginalia.el
@@ -0,0 +1,14 @@
+;; Enable richer annotations using the Marginalia package
+(use-package marginalia
+  :ensure t
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
