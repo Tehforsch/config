bd4a91df0ddc0f1f58e8747bad871ea4bb792102
tehforsch <mail@tonipeter.de>
2021-12-13 20:06:21 +0100

    use helm-find-files instead of dired when opening on directory

--- emacs/helm.el
+++ emacs/helm.el
@@ -4,3 +4,5 @@
 ; This is so ugly but the commented line underneath does not take precedence and im not sure how to do this otherwise.
 (define-key evil-insert-state-map (kbd "C-k") 'helm-previous-line)
 ; (define-key helm-map (kbd "C-k") 'helm-previous-line)
+
+(setq find-directory-functions 'helm-find-files-1)
