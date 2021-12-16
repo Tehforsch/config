2805f7dd324a771ff8c31f6c21194e7fef0e1ce4
tehforsch <mail@tonipeter.de>
2021-12-16 16:19:49 +0100

    remove unused files

--- scripts/syncRepos.sh
+++ scripts/syncRepos.sh
@@ -2,8 +2,6 @@ while [[ 1 ]]; do
     echo "Syncing"
     cd /home/$USER/notes
     ./update.sh
-    cd /home/$USER/projects/tearexCorpTodo
-    ./update.sh
     cd /home/$USER/music
     ./update.sh
     mpc update
