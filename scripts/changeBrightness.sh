de4e05a53cacb19a91fff2c5989baf32bc10f6d1
tehforsch <mail@tonipeter.de>
2021-12-25 00:50:56 +0100

    fix brightness adjustment on laptop where xbacklight does not work for some reason

--- scripts/changeBrightness.sh
+++ scripts/changeBrightness.sh
@@ -1,18 +1,17 @@
-if [ $1 -eq 1 ]; then
-    xbacklight -inc 10
+# Taken from edi9999 from https://stackoverflow.com/questions/23866335/arch-xbacklight-no-outputs-have-backlight-property
+file=/sys/class/backlight/intel_backlight/brightness
+current=$(cat "$file")
+new="$current"
+if [ "$1" = "-inc" ]
+then
+    new=$(( current + $2 ))
 fi
-brightFloat=$(xbacklight)
-brightInt=${brightFloat%.*}
-if [ $1 -eq -1 ]; then
-    if [ $brightInt -gt 10 ]; then
-        xbacklight -dec 10
-    fi
+if [ "$1" = "-dec" ]
+then
+    new=$(( current - $2 ))
 fi
-if [ $1 -eq 0 ]; then
-    if [ $brightInt -eq 0 ]; then
-        xbacklight -set 100
-    else
-        xbacklight -set 0
-    fi
+if [ "$1" = "-set" ]
+then
+    new=$2
 fi
-echo $(xbacklight)
+echo "$new" | tee "$file"
