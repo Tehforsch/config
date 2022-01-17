f54e175a1a7c864558a2547dd621bb52fc6c3072
tehforsch <mail@tonipeter.de>
2022-01-17 20:12:02 +0100

    modify task adding script

--- scripts/addTask.sh
+++ scripts/addTask.sh
@@ -2,12 +2,12 @@ if [[ $# == 0 ]]; then
     read taskText
 else
     taskText="$@"
-    echo "$taskText"
-    if [[ $taskText == *"proj:"* ]]; then
-        task add "$taskText"
-        exit 0
-    fi
 fi
-projects=$(task rc.context=none allProjects | head -n -3 | tail -n +4 | uniq)
-project=$(echo "$projects" | fzf)
-task add proj:$project $taskText
+if [[ $taskText == *"proj:"* ]]; then
+    task add "$taskText"
+    exit 0
+else
+    projects=$(task rc.context=none allProjects | head -n -3 | tail -n +4 | uniq)
+    project=$(echo "$projects" | fzf)
+    task add proj:$project $taskText
+fi
