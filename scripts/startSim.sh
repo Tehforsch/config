8389a41570bbb8a78df4175d7413337ab391ef47
tehforsch <mail@tonipeter.de>
2022-03-08 13:25:54 +0100

    add start sim script

---
+++ scripts/startSim.sh
@@ -0,0 +1,9 @@
+simFolder="$HOME/projects/phd/simulations/"
+workFolder="$WORK"
+path=$(realpath --relative-to "$simFolder" "$1")
+target="$workFolder/$path"
+mkdir -p $(dirname "$target")
+echo "Running in $target"
+bob start "$1" "$target"
+
+
