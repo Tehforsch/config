c8c13627a457012d994371e81d09aae593f056cc
tehforsch <mail@tonipeter.de>
2022-03-08 18:50:41 +0100

    allow start sim script to delete the present sim

--- scripts/startSim.sh
+++ scripts/startSim.sh
@@ -1,9 +1,15 @@
+delete=""
+while [[ "$#" -gt 1 ]]; do
+    case $1 in
+        -d|--delete) delete="-d"; shift ;;
+        *) shift; break;
+    esac
+done
+
 simFolder="$HOME/projects/phd/simulations/"
 workFolder="$WORK"
 path=$(realpath --relative-to "$simFolder" "$1")
 target="$workFolder/$path"
 mkdir -p $(dirname "$target")
 echo "Running in $target"
-$bob start "$1" "$target"
-
-
+$bob start "$delete" "$1" "$target"
