b84b1e68390b2236eb83d41a36266c9688f11b4a
tehforsch <mail@tonipeter.de>
2022-03-29 10:25:32 +0200

    fix history still not working

--- zsh/history.sh
+++ zsh/history.sh
@@ -1,10 +1,10 @@
 export HISTFILE=~/.zsh_history
-export HISTFILESIZE=10000000
-export HISTSIZE=10000000
+export HISTFILESIZE=1000000
+export HISTSIZE=1000000
+export SAVEHIST=1000000
 
 # Remove duplicates
 setopt HIST_IGNORE_ALL_DUPS
 # Allow sharing of history
-setopt append_history
 setopt share_history
 setopt inc_append_history
