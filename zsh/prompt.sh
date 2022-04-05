ce3de09b4dcaf5d4b167c223f525fdb495d95dc9
tehforsch <mail@tonipeter.de>
2022-04-05 17:21:25 +0200

    add zsh git branch prompt thats hopefully faster than oh my zsh was

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,2 +1,25 @@
+function get_git_info()
+{
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == "true" ]]; then
+        branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+        if [[ $(git diff --stat) == '' ]]; then
+            color="6"
+            dirtyString=""
+        else
+            color="11"
+            dirtyString="*"
+        fi
+        if [[ $branch == "" ]];
+        then
+            :
+        else
+            echo '%F{'${color}'}('${dirtyString}${branch}')%f'
+        fi
+    fi
+}
+
+setopt prompt_subst
+
 precmd() { print "" }
-PROMPT=$'%B%F{3}%~%f%b \n> '
+
+PROMPT=$'%B%F{4}%~%f%b $(get_git_info)\n> '
