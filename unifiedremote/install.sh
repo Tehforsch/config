68fc7c2d3d6f3255bdbe40d78d98841b699094ec
tehforsch <mail@tonipeter.de>
2022-04-13 00:13:20 +0200

    add unified remote custom remote

---
+++ unifiedremote/install.sh
@@ -0,0 +1,3 @@
+# this only installs the custom remote for ur, not ur itself
+# restart server (on the dashboard if needed) in order to activate this remote
+sudo ln -s $CONFIG/unifiedremote/custom/ /opt/urserver/remotes/custom 
