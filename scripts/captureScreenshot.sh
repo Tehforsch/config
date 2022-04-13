2437d3adc5cc276989a0f20604e2f801aca5e6cc
tehforsch <mail@tonipeter.de>
2022-04-13 15:43:27 +0200

    remove unused script

--- scripts/captureScreenshot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tempScreenshotFile=~/.tempScreenshot.png
-scrot -s $tempScreenshotFile
-# Turn white transparent
-convert $tempScreenshotFile -transparent white $1
-rm $tempScreenshotFile
