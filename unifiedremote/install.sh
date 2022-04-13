5d6d8e2e3a815f5877a7f0e802df21f3225313a6
tehforsch <mail@tonipeter.de>
2022-04-13 15:35:56 +0200

    fix unified remote install script

--- unifiedremote/install.sh
+++ unifiedremote/install.sh
@@ -1,3 +1,5 @@
 # this only installs the custom remote for ur, not ur itself
 # restart server (on the dashboard if needed) in order to activate this remote
-sudo ln -s $CONFIG/unifiedremote/custom/ /opt/urserver/remotes/custom 
+if [[ ! -d /opt/urserver/remotes/custom ]]; then
+    sudo ln -s $CONFIG/unifiedremote/custom /opt/urserver/remotes/
+fi
