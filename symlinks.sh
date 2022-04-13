b93e4fa8c64ae76311f83c7ccb24c4f7d664ffa2
tehforsch <mail@tonipeter.de>
2022-04-13 16:49:54 +0200

    add emacs for all kinds of file associations

--- symlinks.sh
+++ symlinks.sh
@@ -27,6 +27,7 @@ make_symlink vim/init.vim .config/nvim/init.vim
 make_symlink zathura/zathurarc .config/zathura/zathurarc
 make_symlink mimetypes/mimeapps.list .config/mimeapps.list
 make_symlink mimetypes/zathura.desktop .local/share/applications/zathura.desktop
+make_symlink mimetypes/emacsclient.desktop .local/share/applications/emacsclient.desktop
 
 # The EVIL, polluting my home directory
 make_symlink alsa/asoundrc .asoundrc
