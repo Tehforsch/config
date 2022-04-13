9316fe08f768d55db6b073de47be45815f1008fb
tehforsch <mail@tonipeter.de>
2022-04-13 15:31:11 +0200

    only install mpdas when the file does not exist

--- mpdas/install.sh
+++ mpdas/install.sh
@@ -1,5 +1,6 @@
-mkdir ~/.config/mpdas
-echo "username = tehforsch" > ~/.config/mpdasrc
-echo "password = PASSWORD" >> ~/.config/mpdasrc
-echo "Fill in password in ~/.config/mpdasrc !"
-sleep 4
+if [[ ! -a ~/.config/mpdasrc ]]; then
+    echo "username = tehforsch" > ~/.config/mpdasrc
+    echo "password = PASSWORD" >> ~/.config/mpdasrc
+    echo "Fill in password in ~/.config/mpdasrc !"
+    sleep 4
+fi
