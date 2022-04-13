1a25540197d51210ab7322bf875df5003e9c62f8
tehforsch <mail@tonipeter.de>
2022-04-13 15:31:29 +0200

    make seafile installation optional

--- seafile/instructions.sh
+++ seafile/instructions.sh
@@ -1,6 +1,6 @@
 # I have no idea why this is not even remotely in the package manager. Its super annoying. Anyways this is basically
 # https://github.com/haiwen/libsearpc/blob/master/INSTALL
-pamac build seafile
+pamac build --no-confirm seafile
 mkdir ~/.seafileClient
 cd ~/.seafileClient
 git clone https://github.com/haiwen/seafile-client
