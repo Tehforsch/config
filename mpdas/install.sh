8730b57ade1304eac81c2cb15be853dc9de89763
tehforsch <mail@tonipeter.de>
2022-04-13 14:31:39 +0200

    use XDG in mpdas

--- mpdas/install.sh
+++ mpdas/install.sh
@@ -1,5 +1,5 @@
-echo "username = tehforsch" > ~/.mpdasrc
-echo "password = PASSWORD" >> ~/.mpdasrc
-echo "Fill in password in mpdasrc!"
+mkdir ~/.config/mpdas
+echo "username = tehforsch" > ~/.config/mpdasrc
+echo "password = PASSWORD" >> ~/.config/mpdasrc
+echo "Fill in password in ~/.config/mpdasrc !"
 sleep 4
-
