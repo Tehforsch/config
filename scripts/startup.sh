03a382cb75aea29cd3c7656acfb4cc613e5ac5bc
tehforsch <mail@tonipeter.de>
2022-04-13 15:43:08 +0200

    remove unused script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ~/projects/config/scripts/screenConfiguration.sh
 ~/projects/config/scripts/keyboardConfiguration.sh
-~/projects/config/scripts/backupNotes.sh
 ~/projects/config/scripts/disableAcceleration.sh
