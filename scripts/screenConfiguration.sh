8c270e248c98c8d96340f1df8bb61ad42bd29fe3
tehforsch <mail@tonipeter.de>
2022-08-03 11:52:52 +0200

    run both screen configurations for ita

--- scripts/screenConfiguration.sh
+++ scripts/screenConfiguration.sh
@@ -14,8 +14,12 @@ elif [[ $SYSTEM_NAME == "manjaroPc" ]]; then
     xrandr --output HDMI-0 --primary --auto --output DVI-D-0 --auto --right-of HDMI-0
     exit 0
 elif [[ $SYSTEM_NAME == "ita" ]]; then
+    # try both because this is ridiculously inconsistent
     main=DP2
     second=eDP1
+    xrandr --output $main --primary --auto --output $second --auto --left-of $main
+    main=DP-2
+    second=eDP-1
 else
     main=DP2
     second=DP1-3
