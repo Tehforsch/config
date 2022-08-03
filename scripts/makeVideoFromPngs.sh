85dc1ebcf2ec6239bc7f70a3721ecc0d82456a0c
tehforsch <mail@tonipeter.de>
2022-08-03 17:06:12 +0200

    add explanatory comment for video

--- scripts/makeVideoFromPngs.sh
+++ scripts/makeVideoFromPngs.sh
@@ -3,4 +3,7 @@ if [[ $# -gt 0 ]]; then
 else
     pngs="*.png"
 fi
-cat $pngs | ffmpeg -f image2pipe -i - output.mp4
+cat $pngs | ffmpeg -framerate 20 -f image2pipe -i - output.mp4
+
+# also useful
+# ffmpeg -framerate 10 -r 10 -f image2 -s 1920x1080 -i sliceWithStarParticles_z_Temperature_0_0%2d.png -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -crf 15  -pix_fmt yuv420p output.mp4
