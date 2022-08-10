b70a87b68b5f483038695b5fe7f328d841d95281
tehforsch <mail@tonipeter.de>
2022-08-10 16:06:41 +0200

    disable power saving when pacman is running

--- scripts/disablePowerSavingWhenSoundIsPlaying.sh
+++ scripts/disablePowerSavingWhenSoundIsPlaying.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 while true; do
     soundPlaying=$(pacmd list-sink-inputs | grep -c "RUNNING") 
+    criticalProgramsRunning=$(pgrep pacman | wc -l) 
+    # Since i am too dumb to use "or" in bash and bash just sucks, here we go:
     if [[ $soundPlaying -ge 1 ]]; then
+        xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true
+    elif [[ $criticalProgramsRunning -ge 1 ]]; then
         xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true
     else
         xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s false
