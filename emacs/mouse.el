af92e447b736dd04cd858c74d4d27343c0784263
tehforsch <mail@tonipeter.de>
2022-10-12 17:51:57 +0200

    disable mouse clicks from registering as evil actions so that the last action cannot be repeated with . anymore

--- emacs/mouse.el
+++ emacs/mouse.el
@@ -1,2 +1,5 @@
 (setq mouse-wheel-progressive-speed nil) ; disable acceleration while scrolling
 (setq mouse-wheel-scroll-amount '(5 ((shift) . 5) ((control) . nil)))
+
+(evil-declare-abort-repeat 'evil-mouse-drag-region)
+(evil-declare-abort-repeat 'mouse-set-point)
