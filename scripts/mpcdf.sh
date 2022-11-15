5b142342f6af0a297e62587e8fe08c65763ead43
tehforsch <mail@tonipeter.de>
2022-11-15 10:15:40 +0100

    add mpcdf login

---
+++ scripts/mpcdf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Exactly not how it should be done but my phone cant possibly be part of my workflow
+# sleep 1
+otp=$(oathtool --totp --base32 $(cat ~/.ssh/mpcdf2fa))
+echo $otp
+# for (( i=0; i<${#otp}; i++ )); do
+#     xdotool key "${otp:$i:1}"
+# done
+# xdotool key return
+# sleep 3
+# pw=$(cat ~/.ssh/bwfor)
+# for line in $pw; do
+#     xdotool key $line
+# done
+# xdotool key return
