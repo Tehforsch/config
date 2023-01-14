c1f67aa3e3ba76e2eb198ba7daf6004b5e28c17f
tehforsch <mail@tonipeter.de>
2023-01-14 23:08:32 +0100

    remove unused files

--- emacs/eldoc.el
+++
@@ -1,9 +0,0 @@
-(global-eldoc-mode -1)
-
-(use-package eldoc-box)
-(setq eldoc-box-clear-with-C-g t)
-
-(defun disable-eldoc-mode ()
-    (eldoc-mode -1))
-
-(add-hook 'rust-mode-hook 'disable-eldoc-mode)
