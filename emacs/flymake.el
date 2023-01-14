eca88178c7cdb0c9129f231493587438a08dd556
tehforsch <mail@tonipeter.de>
2023-01-14 16:04:11 +0100

    make error popups less annoying

--- emacs/flymake.el
+++ emacs/flymake.el
@@ -5,7 +5,8 @@
   :config
   (add-hook 'flymake-mode-hook #'flymake-diagnostic-at-point-mode))
 
-(setq flymake-diagnostic-at-point-timer-delay nil)
+(setq flymake-diagnostic-at-point-timer-delay 0.0)
+(setq flymake-diagnostic-at-point-display-diagnostic-function 'flymake-diagnostic-at-point-display-minibuffer)
 
 (defun flymake-diagnostic-at-point-display-popup-now ()
   (interactive)
