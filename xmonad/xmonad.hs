836850a3f595a19a54978c3ed7db607539bf4d54
tehforsch <mail@tonipeter.de>
2023-01-30 23:32:26 +0100

    update xmonad config, include media mode

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -23,6 +23,8 @@ import qualified XMonad.StackSet as W
 
 import qualified Data.Map as M
 
+myScriptFolder = "~/projects/config/scripts/"
+
 myWorkspaces = ["a","s","d","f","g","y","x","c","m"]
 
 myXmobarPP = def
@@ -83,45 +85,83 @@ normalMode = makeMode "Normal"
   [
     ("j", windows W.focusDown),
     ("k", windows W.focusUp),
-    ("f", windows W.focusMaster),
-    ("S-J", rotFocusedDown),
-    ("S-K", rotFocusedUp),
+    ("f", windows W.swapMaster),
+    ("m", windows W.focusMaster),
+    ("S-j", windows W.swapDown),
+    ("S-k", windows W.swapUp),
     ("s", setMode "Launch"),
     ("w", setMode "Workspace"),
+    ("y", setMode "Media"),
     ("q", kill)
   ]
 
-spawnAndExitLaunchMode s = spawn s *> exitModeAndClearLabel
+spawnAndExitMode s = spawn s *> exitModeAndClearLabel
+
+runScript s = spawn ("bash " ++ myScriptFolder ++ s)
+
+runScriptAndExitMode s = runScript s *> exitModeAndClearLabel
 
 launchMode :: Mode
 launchMode = makeMode "Launch"
   [ 
-    ("a", spawnAndExitLaunchMode "kitty --detach --hold taskwarrior-tui"),
-    ("b", spawnAndExitLaunchMode "exec $scripts/bwfor.sh"),
-    ("c", spawnAndExitLaunchMode "firefox"),
-    ("e", spawnAndExitLaunchMode "emacsclient -c -n"),
-    ("p", spawnAndExitLaunchMode "emacsclient -c -n -e '(open-project-alongside-terminal)'"),
-    ("l", spawnAndExitLaunchMode "i3lock -c 000000"),
-    ("m", spawnAndExitLaunchMode "thunderbird"),
-    ("n", spawnAndExitLaunchMode "pcmanfm --no-desktop"),
-    ("s", spawnAndExitLaunchMode "rofi -show run"),
-    ("t", spawnAndExitLaunchMode "telegram-desktop"),
-    ("k", spawnAndExitLaunchMode "kitty"),
-    ("v", spawnAndExitLaunchMode "/opt/cisco/anyconnect/bin/vpnui"),
-    ("z", spawnAndExitLaunchMode "zotero"),
-    ("S+N", spawnAndExitLaunchMode "kitty -o font_size=20 newsboat -u $config/newsboat/urls -C $config/newsboat/config"),
-    ("S+P", spawnAndExitLaunchMode "gpaste-client ui"),
-    ("S+S", spawnAndExitLaunchMode "flameshot gui"),
-    ("<F11>", spawnAndExitLaunchMode "bash /home/toni/projects/config/scripts/screenCapture.sh")
+    ("a", spawnAndExitMode "kitty --detach --hold taskwarrior-tui"),
+    ("b", runScriptAndExitMode "bwfor.sh"),
+    ("c", spawnAndExitMode "firefox"),
+    ("e", spawnAndExitMode "emacsclient -c -n"),
+    ("p", spawnAndExitMode "emacsclient -c -n -e '(open-project-alongside-terminal)'"),
+    ("l", spawnAndExitMode "i3lock -c 000000"),
+    ("m", spawnAndExitMode "thunderbird"),
+    ("n", spawnAndExitMode "pcmanfm --no-desktop"),
+    ("s", spawnAndExitMode "rofi -show run"),
+    ("t", spawnAndExitMode "telegram-desktop"),
+    ("k", spawnAndExitMode "kitty"),
+    ("v", spawnAndExitMode "/opt/cisco/anyconnect/bin/vpnui"),
+    ("z", spawnAndExitMode "zotero"),
+    ("S-n", spawnAndExitMode "kitty -o font_size=20 newsboat -u $config/newsboat/urls -C $config/newsboat/config"),
+    ("S-p", spawnAndExitMode "gpaste-client ui"),
+    ("S-s", spawnAndExitMode "flameshot gui"),
+    ("<F11>", runScriptAndExitMode "screenCapture.sh")
   ]
 
 workspaceMode :: Mode
 workspaceMode = makeMode "Workspace"
   [(workspace, windows $ W.greedyView workspace) | workspace <- myWorkspaces]
 
+mediaMode :: Mode
+mediaMode = makeMode "Media"
+  [
+    ("a", spawn "mpc prev"),
+    ("d", spawn "mpc next"),
+    ("f", spawn "pactl set-sink-volume @DEFAULT_SINK@ -4%"),
+    ("r", spawn "pactl set-sink-volume @DEFAULT_SINK@ +4%"),
+    ("l", runScript "showLyrics.sh"),
+    ("s", spawn "mpc toggle"),
+    ("g", runScript "changeBrightness.sh -dec 300"),
+    ("t", runScript "changeBrightness.sh -inc 300"),
+    ("j", spawn "mpc seek +10"),
+    ("k", spawn "mpc seek -10"),
+    ("S-f", spawn "mpc volume -5"),
+    ("S-r", spawn "mpc volume +5"),
+    ("S-o", runScriptAndExitMode "screenConfiguration.sh"),
+    ("S-d", spawnAndExitMode "redshift -O 3500 -b 0.4"),
+    ("m", setMode "Find Music")
+  ]
+
+findMusicMode :: Mode
+findMusicMode = makeMode "Find Music"
+  [
+    ("k", runScriptAndExitMode "musicSelection/artist.sh"),
+    ("a", runScriptAndExitMode "musicSelection/album.sh"),
+    ("s", runScriptAndExitMode "musicSelection/song.sh"),
+    ("r", runScript "musicSelection/randomAlbum.sh"),
+    ("t", runScriptAndExitMode "musicSelection/newSongThisAlbum.sh"),
+    ("S-a", runScriptAndExitMode "musicSelection/quarantineAlbum.sh"),
+    ("S-r", runScript "musicSelection/quarantineAlbum.sh --random")
+  ]
+
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh
-  $ modal [normalMode, launchMode, defaultMode, workspaceMode]
+  $ modal [normalMode, launchMode, defaultMode, workspaceMode, mediaMode, findMusicMode]
   $ withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
   $ myConfig
 
