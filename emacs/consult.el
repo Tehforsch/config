d670c05de797572388ecd756c645a7dab10b9a24
tehforsch <mail@tonipeter.de>
2023-03-30 12:05:00 +0200

    fix consult-rg results showing up twice

--- emacs/consult.el
+++ emacs/consult.el
@@ -12,7 +12,7 @@
 
 (setq consult-project-root-function #'projectile-project-root)
 
-(setq consult-ripgrep-args "rg --color=never --path-separator / --smart-case --no-heading --line-number --line-buffered --null .")
+(setq consult-ripgrep-args "rg --color=never --path-separator / --smart-case --no-heading --line-number --line-buffered --null")
 
 ; Make xref show xrefs and xref show definitions use completion-at-point / consult
 (setq xref-show-xrefs-function 'consult-xref)
