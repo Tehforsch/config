b0e652d57aeca597a104afa37fee6758679a3fd1
tehforsch <mail@tonipeter.de>
2023-04-18 22:00:02 +0200

    Improve flamegraph script

--- scripts/flamegraphRunningProcess.sh
+++ scripts/flamegraphRunningProcess.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ps -e | fzf | awk '{print $1; exit}' | xargs flamegraph -o ~/.local/share/tmp/$(date +%Y-%m-%d-%H-%M-%S).svg --open --pid
+folder="$(realpath $HOME/.local/share/tmp)/flamegraphs"
+mkdir -p "$folder"
+cd "$folder"
+ps -e | fzf | awk '{print $1; exit}' | xargs flamegraph -o "$folder/$(date +%Y-%m-%d-%H-%M-%S).svg" --open --pid
