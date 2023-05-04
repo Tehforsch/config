17109b4bdf371e8581a93f36541b0baddc0c1969
tehforsch <mail@tonipeter.de>
2023-05-04 23:08:31 +0200

    use hotspot in flamegraph script

--- scripts/flamegraphRunningProcess.sh
+++ scripts/flamegraphRunningProcess.sh
@@ -2,4 +2,5 @@
 folder="$(realpath $HOME/.local/share/tmp)/flamegraphs"
 mkdir -p "$folder"
 cd "$folder"
-ps -e | fzf | awk '{print $1; exit}' | xargs flamegraph -o "$folder/$(date +%Y-%m-%d-%H-%M-%S).svg" --open --pid
+ps -e | fzf | awk '{print $1; exit}' | xargs perf record --call-graph dwarf --pid
+hotspot perf.data 2> /dev/null
