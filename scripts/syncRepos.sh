765b3583a884b59c0739a4446625430e84085bea
tehforsch <mail@tonipeter.de>
2023-09-04 11:42:16 +0200

    sync tw

--- scripts/syncRepos.sh
+++ scripts/syncRepos.sh
@@ -1,14 +1,5 @@
 while [[ 1 ]]; do
     echo "Syncing"
     task sync
-    cd /home/$USER/notes
-    ./update.sh
-    cd /home/$USER/music
-    ./update.sh
-    mpc update
-    cd /home/$USER/pictures
-    ./update.sh
-    cd /home/$USER/finances
-    ./update.sh
     sleep 600
 done
