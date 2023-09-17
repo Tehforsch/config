6d009008b8900c3331e7eb9dc03a4a97b3526ff0
tehforsch <mail@tonipeter.de>
2023-09-17 19:46:11 +0200

    since spotify is the piece of garbage software that creates the Downloads folder (people are 'working on it' since 2020), i dont need the script anymore. i replaced spotify by a script that runs the binary and removes the download folder

--- scripts/findOutWhoCreatesDownloads.sh
+++
@@ -1,8 +0,0 @@
-while [[ 1 ]]; do
-    if [[ -d ~/Downloads ]]; then
-        notify-send "SOMEBODY CREATED DOWNLOADS FOLDER JO"
-        ps -ef > ~/.playground/WHO_IS_AT_FAULT_$(date +%s)
-        sleep 600
-    fi
-    sleep 5
-done
