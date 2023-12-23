fa8ab3c07732d3d3e6e0a0383c2f0a671142d4ab
tehforsch <mail@tonipeter.de>
2023-12-23 17:11:17 +0100

    update for new install

--- emacs/cape.el
+++ emacs/cape.el
@@ -1,7 +1,7 @@
 (use-package cape)
 
-(defalias 'cape-dabbrev+lsp+file
-  (cape-super-capf
-    #'cape-dabbrev
-    #'lsp-completion-at-point
-    #'cape-file))
+; (defalias 'cape-dabbrev+lsp+file
+;   (cape-super-capf
+;     #'cape-dabbrev
+;     #'lsp-completion-at-point
+;     #'cape-file))
