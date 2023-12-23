07b581451ba473f4b7c5b9cccb6fa33991060f6f
tehforsch <mail@tonipeter.de>
2023-12-23 21:13:28 +0100

    fix disable-acceleration script not working for multiple mouse ids

--- scripts/disableAcceleration.sh
+++ scripts/disableAcceleration.sh
@@ -3,9 +3,11 @@ for id in $(seq 11 20); do
     xinput set-prop $id "libinput Accel Speed" -0.0
 done
 # Disable acceleration for mouse
-mouseId=$(xinput list | grep Mouse | grep pointer | grep -v Consumer |  cut -f 2 | sed "s/id=//")
-echo $mouseId
-xinput set-prop $mouseId "libinput Accel Speed" -1.0
+mouseIds=$(xinput list | grep Mouse | grep pointer | grep -v Consumer |  cut -f 2 | sed "s/id=//")
+for mouseId in $mouseIds; do
+    echo $mouseId
+    xinput set-prop $mouseId "libinput Accel Speed" -1.0
 
-mouseSpeed=1.00
-xinput set-prop $mouseId "Coordinate Transformation Matrix" $mouseSpeed 0 0 0 $mouseSpeed 0 0 0 1
+    mouseSpeed=1.00
+    xinput set-prop $mouseId "Coordinate Transformation Matrix" $mouseSpeed 0 0 0 $mouseSpeed 0 0 0 1
+done
