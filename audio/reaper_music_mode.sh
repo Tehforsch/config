635f64a0f6842e475df918dbcd0ee021e195b762
tehforsch <mail@tonipeter.de>
2024-01-18 11:46:11 +0100

    update reaper starting script to wait for audio interface

--- audio/reaper_music_mode.sh
+++ audio/reaper_music_mode.sh
@@ -1,4 +1,9 @@
+# If we start reaper before the interface is turned on
+# it sets the connections up wrong
 sudo cpupower frequency-set -g performance
+while [ $(lsusb | grep BEHRINGER | wc -l) == 0 ]; do
+    sleep 0.1
+done
 $CONFIG/audio/wireplumber_music_mode.sh
 sleep 1
 PIPEWIRE_LATENCY=“32/48000” pw-jack -p64 reaper
