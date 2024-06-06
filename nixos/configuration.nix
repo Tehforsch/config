e14bd2a4ebdbf4ca4b25a2da930240eba16ad3ff
tehforsch <mail@tonipeter.de>
2024-06-06 13:05:07 +0200

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,7 +6,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "toniframework"; # Define your hostname.
+  networking.hostName = "framework"; # Define your hostname.
   networking.networkmanager.enable = true;
 
   # Set your time zone.
