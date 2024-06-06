2a7385eddc7891e57553091efe1c7a1a6bc7acae
tehforsch <mail@tonipeter.de>
2024-06-06 11:16:08 +0200

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -120,7 +120,7 @@
   programs.openvpn3.enable = true;
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
