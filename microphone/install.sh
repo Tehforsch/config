81841f26a9e52266c0b43c2a0de0b9f6f84fe85a
tehforsch <mail@tonipeter.de>
2024-06-06 11:31:25 +0200

    first wave of removals

--- microphone/install.sh
+++
@@ -1,2 +0,0 @@
-sudo echo 'load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args="analog_gain_control=0\ digital_gain_control=1" source_name=echoCancel_source sink_name=echoCancel_sink' >> /etc/pulse/default.pa
-sudo echo 'set-default-source echoCancel_source' >> /etc/pulse/default.pa
