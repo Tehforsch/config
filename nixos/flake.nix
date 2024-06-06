1d58d8beed243b89097baf3e94a1b682fe3d618d
tehforsch <mail@tonipeter.de>
2024-06-06 13:05:07 +0200

    update

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs@{ self, nixpkgs, ... }: {
     nixosConfigurations = {
-      toniframework = nixpkgs.lib.nixosSystem {
+      framework = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
