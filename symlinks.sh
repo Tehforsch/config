0109657fd48452a7f1d670425e13611f1b27ca67
tehforsch <mail@tonipeter.de>
2024-06-06 12:05:51 +0200

    update

--- symlinks.sh
+++ symlinks.sh
@@ -24,7 +24,6 @@ function make_symlink {
 
 # The good, following the XDG_CONFIG_DIR structure
 make_symlink beets/config.yaml .config/beets/config.yaml
-make_symlink bacon/prefs.toml .config/bacon/prefs.toml
 make_symlink bat/config .config/bat/config
 make_symlink i3/i3.conf .config/i3/config
 make_symlink i3/${SYSTEM_NAME}.conf .config/i3/system.conf
@@ -32,43 +31,25 @@ make_symlink i3/i3wsr/config.toml .config/i3wsr/config.toml
 make_symlink i3/i3status.conf .config/i3status/config
 make_symlink kitty/kitty.conf .config/kitty/kitty.conf
 make_symlink mpd/${SYSTEM_NAME}.conf .config/mpd/mpd.conf
-make_symlink mypy/mypy.ini .config/mypy/config
-make_symlink nushell/config.nu .config/nushell/config.nu
 make_symlink rofi/config.rasi .config/rofi/config.rasi
 make_symlink vim/init.vim .config/nvim/init.vim
 make_symlink zathura/zathurarc .config/zathura/zathurarc
-make_symlink mimetypes/mimeapps.list .config/mimeapps.list
-make_symlink powersettings/${SYSTEM_NAME}.xml .config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
 make_symlink emacs/emacs.service .config/systemd/user/default.target.wants/emacs.service
 make_symlink taskwarrior/taskrc .config/task/taskrc
 make_symlink taskwarrior/hooks .local/share/task/hooks
-make_symlink xmonad/xmonad.hs .config/xmonad/xmonad.hs
-make_symlink xmonad/lib/XMobarConfig.hs .config/xmonad/lib/XMobarConfig.hs
-make_symlink xmonad/lib/Colors.hs .config/xmonad/lib/Colors.hs
-make_symlink xmonad/xmobarrc .config/xmonad/xmobarrc
 make_symlink zsh/zshrc.sh .config/zsh/.zshrc # ZDOTDIR
 make_symlink bash/inputrc.sh .config/readline/inputrc # INPUTRC
 make_symlink newsboat/config .config/newsboat/config
 make_symlink newsboat/urls .config/newsboat/urls
 make_symlink git/gitconfig.conf .config/git/config
-make_symlink alsa/asoundrc .config/alsa/asoundrc
 make_symlink vim/init.vim .config/vim/vimrc
 make_symlink cargo/${SYSTEM_NAME}.toml .cargo/config.toml # ...
-make_symlink leftwm/config.ron .config/leftwm/config.ron
-make_symlink leftwm/lefthk.ron .config/lefthk/config.ron
-# make_symlink leftwm/startup_script.desktop .config/autostart/startup_script.desktop
-make_symlink sxhkd/sxhkdrc .config/sxhkd/sxhkdrc
+make_symlink wireplumber/51-alsa-disable-music-mode.lua .config/wireplumber/main.lua.d/51-alsa-disable.lua
 
 # The weird
-make_symlink mimetypes/zathura.desktop .local/share/applications/zathura.desktop
-make_symlink mimetypes/emacsclient.desktop .local/share/applications/emacsclient.desktop
-make_symlink mimetypes/hdfview.desktop .local/share/applications/hdfview.desktop
-make_symlink zsh/openPdf.sh .local/bin/pdf
 make_symlink taskwarrior/${SYSTEM_NAME}.conf .config/task/system.conf
-make_symlink wireplumber/51-alsa-disable-music-mode.lua .config/wireplumber/main.lua.d/51-alsa-disable.lua
 
 # The EVIL, polluting my home directory
 make_symlink bash/bashrc.sh .bashrc
 make_symlink emacs/init.el .emacs.d/init.el
-make_symlink gnuplot/gnuplot.conf .gnuplot # supports XDG starting at version 5.5 which is not on manjaro stable
 make_symlink ssh/config .ssh/config
