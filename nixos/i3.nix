fcbb6c4a5b8caaa7ecf5c3e9d64332fbfcf673d3
tehforsch <mail@tonipeter.de>
2024-06-11 13:41:57 +0200

    update

--- nixos/i3.nix
+++ nixos/i3.nix
@@ -7,10 +7,6 @@
       xterm.enable = false;
     };
    
-    displayManager = {
-        defaultSession = "none+i3";
-    };
-
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
@@ -18,7 +14,14 @@
         i3lock
      ];
     };
+
+    xkb.layout = "de";
+    xkb.variant = "nodeadkeys";
   };
+  services.displayManager = {
+    defaultSession = "none+i3";
+  };
+
   environment.systemPackages = with pkgs; [
     rofi
   ];
