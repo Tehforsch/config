eb31ee3f3679ea463fa4aff6e8f9cde118cd3055
tehforsch <mail@tonipeter.de>
2024-06-12 19:46:53 +0200

    refactor flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,34 +15,32 @@
   };
 
   outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations = {
-      framework = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [
-          { networking.hostName = "framework"; }
-          ./configuration.nix
-          ./keyboard-configuration.nix
-          # ./hyprland.nix
-          ./i3.nix
-          ./hardware-framework.nix
-          ./redshift.nix
-        ];
+    nixosConfigurations = let modules = [
+        ./configuration.nix
+        ./keyboard-configuration.nix
+        # ./hyprland.nix
+        ./i3.nix
+        ./redshift.nix
+    ];
+    in
+      {
+        framework = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = [
+            { networking.hostName = "framework"; }
+            ./hardware-framework.nix
+          ] ++ modules;
+        };
+        pc = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = [
+            { networking.hostName = "pc"; }
+            ./hardware-pc.nix
+            ./custom-pc.nix
+          ] ++ modules;
+        };
       };
-      pc = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [
-          { networking.hostName = "pc"; }
-          ./configuration.nix
-          ./keyboard-configuration.nix
-          # ./hyprland.nix
-          ./i3.nix
-          ./hardware-pc.nix
-          ./custom-pc.nix
-          ./redshift.nix
-        ];
-      };
-    };
   };
 }
