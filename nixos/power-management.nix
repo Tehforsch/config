a630c2e69080489924899f4638f25aecf3ffdc93
tehforsch <mail@tonipeter.de>
2024-06-29 12:08:55 +0200

    power management

--- nixos/power-management.nix
+++ nixos/power-management.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }:
 {
-  systemd.sleep.extraConfig = ''
-    HibernateDelaySec=1min
+  # systemd.sleep.extraConfig = ''
+  #   IdleActionSec=300s
+  #   IdleAction=suspend
+  # '';
+
+  services.logind.extraConfig = ''
+    IdleActionSec=30s
+    IdleAction=suspend
   '';
 }
