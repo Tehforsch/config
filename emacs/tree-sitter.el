a94a5fdb09ed049a62eb0a9d45b40197fe321e95
Toni Peter <mail@tonipeter.de>
2024-08-01 20:44:40 +0200

    ignore direnv

--- emacs/tree-sitter.el
+++ emacs/tree-sitter.el
@@ -19,22 +19,15 @@
 
 (setq treesit-font-lock-level 4)
 
-; Everytime I check this again virtually none of the textobjects work for me :|
-(use-package evil-textobj-tree-sitter :ensure t)
+;; ; Everytime I check this again virtually none of the textobjects work for me :|
+;; (use-package evil-textobj-tree-sitter :ensure t)
 
-(define-key evil-outer-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.outer"))
-(define-key evil-inner-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.inner"))
+;; (define-key evil-outer-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.outer"))
+;; (define-key evil-inner-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.inner"))
 
-(define-key evil-outer-text-objects-map "c" (evil-textobj-tree-sitter-get-textobj "class.outer"))
-(define-key evil-inner-text-objects-map "c" (evil-textobj-tree-sitter-get-textobj "class.inner"))
+;; (define-key evil-outer-text-objects-map "c" (evil-textobj-tree-sitter-get-textobj "class.outer"))
+;; (define-key evil-inner-text-objects-map "c" (evil-textobj-tree-sitter-get-textobj "class.inner"))
 
-; Overwrite this to make sure even rustic mode gets the message that
-;we are using builtin treesitter
-(defun  evil-textobj-tree-sitter--use-builtin-treesitter () t)
-
-;; Goto start of next function
-(define-key evil-normal-state-map
-            (kbd "]f")
-            (lambda ()
-              (interactive)
-              (evil-textobj-tree-sitter-goto-textobj "parameters.outer")))
+;; ; Overwrite this to make sure even rustic mode gets the message that
+;; ;we are using builtin treesitter
+;; (defun  evil-textobj-tree-sitter--use-builtin-treesitter () t)
