2a664e74cf079b1e3058439a774c782173de7998
Toni Peter <mail@tonipeter.de>
2024-08-21 11:11:00 +0200

    adjust work vpn for thinkpad

--- scripts/workVpn.sh
+++ scripts/workVpn.sh
@@ -1,2 +1,6 @@
-nmcli con up id "Greenbone VPN"
-sudo ip link set mtu 1380 dev gb-vpn
+if [[ $(hostname) == thinkpad ]]; then
+    sudo openconnect vpn.greenbone.net --protocol=gp
+else
+    nmcli con up id "Greenbone VPN"
+    sudo ip link set mtu 1380 dev gb-vpn
+fi
