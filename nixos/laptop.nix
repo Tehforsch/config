878693a0ee0e34c503d6ba23c71da1da8ed70208
Toni Peter <mail@tonipeter.de>
2024-08-21 11:09:36 +0200

    add bluetooth

--- nixos/laptop.nix
+++ nixos/laptop.nix
@@ -28,4 +28,19 @@
   };
 
   services.fwupd.enable = true;
+
+  services.blueman.enable = true;
+  environment.systemPackages = with pkgs; [bluez bluez-tools];
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings.General = {
+      Enable = "Source,Sink,Media,Socket";
+      Name = "Hello";
+      ControllerMode = "dual";
+      FastConnectable = "true";
+      Experimental = "true";
+      KernelExperimental = "true";
+    };
+  };
 }
