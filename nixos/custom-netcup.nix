a9d50ededba51a15bf2534406896434fc5dd0a22
Toni Peter <mail@tonipeter.de>
2024-11-25 15:58:22 +0100

    add murmur

--- nixos/custom-netcup.nix
+++ nixos/custom-netcup.nix
@@ -9,4 +9,9 @@
     settings.server.hosts = [ "0.0.0.0:5232" ];
   };
   networking.firewall.allowedTCPPorts = [ 80 443 5232 ];
+
+  services.murmur = {
+    enable = true;
+    openFirewall = true;
+  };
 }
