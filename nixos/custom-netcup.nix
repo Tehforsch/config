cb8913090c377869e9db9217e3d872b06c9e4055
Toni Peter <mail@tonipeter.de>
2024-11-25 09:04:29 +0100

    add radicale

--- nixos/custom-netcup.nix
+++ nixos/custom-netcup.nix
@@ -3,4 +3,10 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = true;
+
+  services.radicale = {
+    enable = true;
+    settings.server.hosts = [ "0.0.0.0:5232" ];
+  };
+  networking.firewall.allowedTCPPorts = [ 80 443 5232 ];
 }
