7ca3706d983647e0e2c72dbca0b7b1417bcde91c
Toni Peter <mail@tonipeter.de>
2024-12-12 10:29:50 +0100

    update audio

--- audio/scripts/reaper_mode.sh
+++ audio/scripts/reaper_mode.sh
@@ -4,6 +4,7 @@
 # If we start reaper before the interface is turned on
 # it sets the connections up wrong
 if [ "$1" == "music" ] || [ "$1" == "amp" ] ; then
+    echo "hi"
     while [ $(lsusb | grep BEHRINGER | wc -l) == 0 ]; do
         sleep 0.1
     done
@@ -11,8 +12,11 @@ if [ "$1" == "music" ] || [ "$1" == "amp" ] ; then
     pactl set-card-profile alsa_card.usb-BEHRINGER_UMC1820_F62F2AF7-00 pro-audio
 fi
 
-# HOME=/home/toni /home/toni/projects/config/audio/scripts/wireplumber_mode.sh $1
+HOME=/home/toni /home/toni/projects/config/audio/scripts/wireplumber_mode.sh $1
 sleep 0.3
-qpwgraph -a -x /home/toni/projects/config/audio/wireplumber/reaper.qpwgraph
-PIPEWIRE_QUANTUM=64/48000 reaper
+PIPEWIRE_QUANTUM=64/48000 reaper &
 
+sleep 5
+
+
+qpwgraph -a -x /home/toni/projects/config/audio/wireplumber/reaper.qpwgraph
