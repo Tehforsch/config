d18e1794c0194fc5ee154084539d96b94c6563b0
Toni Peter <mail@tonipeter.de>
2025-01-16 12:25:39 +0100

    add homebox

--- nixos/custom/pc.nix
+++ nixos/custom/pc.nix
@@ -50,6 +50,14 @@
       PAPERLESS_ADMIN_USER = "toni";
     };
   };
+
+  services.homebox = {
+    enable = true;
+    settings = {
+      HBOX_STORAGE_DATA = "/var/lib/homebox/data";
+      HBOX_STORAGE_SQLITE_URL = "/var/lib/homebox/data/homebox.db?_pragma=busy_timeout=999&_pragma=journal_mode=WAL&_fk=1";
+      HBOX_OPTIONS_ALLOW_REGISTRATION = "false";
+      HBOX_MODE = "production";
+    };
+  };
 }
-
-
