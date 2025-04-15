c892d3225a2fdd1d3ff18a3f4abdcb364f413a07
Toni Peter <mail@tonipeter.de>
2025-04-15 16:59:53 +0200

    fix script

--- scripts/runLastCommandInTerminalToTheRight.sh
+++ scripts/runLastCommandInTerminalToTheRight.sh
@@ -1,13 +1,4 @@
-xdotool key --clearmodifiers F9
-xdotool key --clearmodifiers l
-xdotool key --clearmodifiers Escape
-sleep 0.08;
+i3-msg focus right
 xdotool key --clearmodifiers ctrl+p
 xdotool key --clearmodifiers Return
-if [[ $# == 0 ]]; then
-    xdotool key F9
-    sleep 0.1
-    xdotool key h
-    sleep 0.1
-    xdotool key Tab
-fi
+i3-msg focus left
