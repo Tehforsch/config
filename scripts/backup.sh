1081ce08a90b9a4e87817fe276782a388dd4057c
Toni Peter <mail@tonipeter.de>
2025-07-28 11:35:30 +0200

    add paperless export to backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,6 +2,7 @@ exthdd=/mnt/extHdd/
 dir=$exthdd/backup
 # mkdir $dir
 # borg init -e repokey $dir
-
+tempdir=~/downloads/paperless_export
+paperless-manage document_exporter "$tempdir"
 date=$(date -I)
-borg create -v "${dir}::$date" ~/resource $exthdd/pictures/important
+borg create -v "${dir}::$date" ~/resource $exthdd/pictures/important "$tempdir"
