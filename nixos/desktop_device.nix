e556b4fe1a0878ca636f9f2fee62f12368044802
Toni Peter <mail@tonipeter.de>
2025-09-12 12:08:26 +0200

    revert wayland #1

--- nixos/desktop_device.nix
+++ nixos/desktop_device.nix
@@ -41,16 +41,5 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [
-        vulkan-loader
-        vulkan-validation-layers
-        vulkan-extension-layer
-        vulkan-tools
-        wayland
-    ];
   };
-
-  environment.systemPackages = with pkgs; [ 
-    vulkan-tools
-  ];
 }
