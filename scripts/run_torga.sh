4c16c91a670b23791e77b58d901a3b74e80a6294
Toni Peter <mail@tonipeter.de>
2025-10-21 13:26:04 +0200

    update

--- scripts/run_torga.sh
+++ scripts/run_torga.sh
@@ -2,5 +2,4 @@
 api_key=$(cat ~/resource/keys/torga/api_key)
 # Temporary fix while we're installing from cargo
 export PATH=$PATH:~/.cargo/bin
-torga-cli --data-path ~/.local/share/todo --config ~/.config/todo/torga.yml --api-key $api_key tui 2>> ~/.playground/out
-echo "$?" >> ~/.playground/out
+torga-cli --data-path ~/.local/share/todo --config ~/.config/todo/torga.yml --api-key $api_key tui
