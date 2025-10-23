2b63c6fe63e097dcfcf471a1daf0b64bcc32079e
Toni Peter <mail@tonipeter.de>
2025-10-23 18:19:05 +0200

    just in case: notmuch

--- symlinks.sh
+++ symlinks.sh
@@ -68,6 +68,8 @@ make_symlink torga/torga.yml .config/todo/torga.yml
 make_symlink rmpc/config.ron .config/rmpc/config.ron
 make_symlink rmpc/theme.ron .config/rmpc/themes/default.ron
 make_symlink mbsync/mbsyncrc .config/isyncrc
+make_symlink notmuch/config .notmuch-config
+make_symlink meli/config.toml .config/meli/config.toml
 
 # The weird
 make_symlink taskwarrior/${SYSTEM_NAME}.conf .config/task/system.conf
