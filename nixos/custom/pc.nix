c5593b74b9551f06ee73b8f4f5be3fcde26c7451
Toni Peter <mail@tonipeter.de>
2025-10-23 12:54:26 +0200

    Copy keepass file once a day

--- nixos/custom/pc.nix
+++ nixos/custom/pc.nix
@@ -20,4 +20,16 @@
     allowedTCPPorts = [ 8080 ];
     allowedUDPPorts = [ 8080 ];
   };
+
+  systemd.services.copy-keepass-db = {
+    description = "Copy KeePass database to phone directory";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "toni";
+      ExecStart = "${pkgs.coreutils}/bin/cp /home/toni/resource/keys/keepass/Passwords.kdbx /home/toni/resource/phone/";
+      RemainAfterExit = true;
+    };
+  };
 }
