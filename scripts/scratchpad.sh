205fa297de3c9216302b13b8e38dfda33126e169
Toni Peter <mail@tonipeter.de>
2025-10-23 08:20:33 +0200

    fix scratchpad handling

--- scripts/scratchpad.sh
+++ scripts/scratchpad.sh
@@ -4,6 +4,6 @@
 focused_class=$(i3-msg -t get_tree | jq -r '.. | select(.focused? == true) | .window_properties.class')
 
 # Check if the focused window is one of the target classes
-if [[ "$focused_class" == "Mattermost" || "$focused_class" == "KeePassXC" ]]; then
+if [[ "$focused_class" == "Mattermost" || "$focused_class" == "KeePassXC" || "$focused_class" == "thunderbird" ]]; then
     i3-msg move scratchpad
 fi
