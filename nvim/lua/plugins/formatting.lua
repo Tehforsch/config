c4d5250f25f5341def3ea998368aada45c83af0e
Toni Peter <mail@tonipeter.de>
2025-12-15 00:33:15 +0100

    add conform

---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,40 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      ",f",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      rust = { "rustfmt" },
+      python = { "black" },
+      nix = { "alejandra" },
+      haskell = { "fourmolu" },
+      lua = { "stylua" },
+      c = { "clang_format" },
+      cpp = { "clang_format" },
+      yaml = { "prettier" },
+      json = { "prettier" },
+      markdown = { "prettier" },
+    },
+    formatters = {
+      rustfmt = {
+        command = "rustfmt",
+        args = { "--edition", "2021" },
+      },
+      clang_format = {
+        command = "clang-format",
+        args = { "-style=file" },
+      },
+    },
+    format_on_save = nil,
+  },
+}
