5ae9d5bf0c79bd2d8fbe4368ea39ae162154e9e9
Toni Peter <mail@tonipeter.de>
2025-12-14 11:57:20 +0100

    Neovim: Gruvbox

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      terminal_colors = true,
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true,
+      contrast = "hard",
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    })
+    vim.o.background = "dark"
+    vim.cmd("colorscheme gruvbox")
+  end,
+}
