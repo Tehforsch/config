b4951279e3be0897c7c00fed0d692a452d2a42af
Toni Peter <mail@tonipeter.de>
2025-12-14 18:31:08 +0100

    Add nvim surround, comment, treesitter objects

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,25 @@
+return {
+  "numToStr/Comment.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("Comment").setup({
+      toggler = {
+        line = "gcc",
+        block = "gbc",
+      },
+      opleader = {
+        line = "gc",
+        block = "gb",
+      },
+      mappings = {
+        basic = true,
+        extra = true,
+      },
+    })
+
+    vim.keymap.set("n", "<C-e>", function()
+      return vim.api.nvim_get_vvar("count") == 0 and "<Plug>(comment_toggle_linewise_current)"
+        or "<Plug>(comment_toggle_linewise_count)"
+    end, { expr = true, desc = "Comment line" })
+  end,
+}
