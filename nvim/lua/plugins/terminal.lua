4241d6534ae190166a3a8f215a17b36e433a1af5
Toni Peter <mail@tonipeter.de>
2025-12-15 00:39:10 +0100

    Add terminal to the right command.

---
+++ nvim/lua/plugins/terminal.lua
@@ -0,0 +1,16 @@
+-- Terminal-related functionality for SPC r command
+-- Calls the existing runLastCommandInTerminalToTheRight.sh script
+
+local script_path = vim.fn.expand("~/projects/config/scripts/runLastCommandInTerminalToTheRight.sh")
+
+local function run_command_in_terminal_to_right()
+  -- Save the current file
+  vim.cmd("write")
+
+  -- Run the bash script
+  vim.fn.system(script_path)
+end
+
+vim.keymap.set("n", "<leader>r", run_command_in_terminal_to_right, { desc = "Save and run last command in right terminal" })
+
+return {}
