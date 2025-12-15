3e8358ec6c44f1ae696b8d933e438b7810ef9ba0
Toni Peter <mail@tonipeter.de>
2025-12-15 10:49:13 +0100

    Add which-key

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      local wk = require("which-key")
+      wk.setup({
+        delay = 500,
+        plugins = {
+          marks = true,
+          registers = true,
+          spelling = {
+            enabled = false,
+          },
+          presets = {
+            operators = false,
+            motions = false,
+            text_objects = false,
+            windows = false,
+            nav = false,
+            z = false,
+            g = false,
+          },
+        },
+      })
+
+      wk.add({
+        { "<leader>f", group = "file" },
+        { "<leader>p", group = "project" },
+        { "<leader>g", group = "git" },
+        { "<leader>u", group = "ui" },
+        { "<leader>h", group = "history" },
+        { "<localleader>", group = "local" },
+      })
+    end,
+  },
+}
