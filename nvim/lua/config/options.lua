fbf04384ec351d79b8750c42994b0d71441bd56d
Toni Peter <mail@tonipeter.de>
2025-12-22 21:39:10 +0100

    Add breadcrumbs
    Squash statusline

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -37,6 +37,5 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.opt.showmode = false
 vim.opt.cmdheight = 1
 
-vim.opt.winbar = "%{%v:lua.require('config.winbar').winbar()%}"
 vim.opt.laststatus = 0
 vim.opt.ruler = false
--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -3,4 +3,24 @@ return {
 		"nvim-lualine/lualine.nvim",
 		enabled = false,
 	},
+	{
+		"Bekaboo/dropbar.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
+		opts = {
+			bar = {
+				enable = function(buf, win)
+					return vim.fn.win_gettype(win) == ""
+						and vim.wo[win].winbar == ""
+						and vim.bo[buf].bt == ""
+						and (
+							vim.bo[buf].ft == "markdown"
+							or (buf and vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buftype == "")
+						)
+				end,
+			},
+		},
+	},
 }
