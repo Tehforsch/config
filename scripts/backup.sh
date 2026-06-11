c85b3c9f76703e3b20037fefdcb3df7d3d34fc25
Toni Peter <mail@tonipeter.de>
2026-06-11 15:06:26 +0200

    Add more things to backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,6 +3,7 @@ dir=$exthdd/backup
 # mkdir $dir
 # borg init -e repokey $dir
 tempdir=~/downloads/paperless_export
+mkdir -p $tempdir
 paperless-manage document_exporter "$tempdir"
 date=$(date -I)
-borg create -v "${dir}::$date" ~/resource $exthdd/archive $tempdir
+borg create -v "${dir}::$date" ~/resource $exthdd/archive $tempdir ~/.local/share/paperless/ ~/.local/share/todo/
