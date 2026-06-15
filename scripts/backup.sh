2f79e4e9645380317073df08523911a19a0e35e2
Toni Peter <mail@tonipeter.de>
2026-06-15 11:08:17 +0200

    Update aerc.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,4 +6,4 @@ tempdir=~/downloads/paperless_export
 mkdir -p $tempdir
 paperless-manage document_exporter "$tempdir"
 date=$(date -I)
-borg create -v "${dir}::$date" ~/resource $exthdd/archive $tempdir ~/.local/share/paperless/ ~/.local/share/todo/
+borg create -v "${dir}::$date" ~/resource $exthdd/archive $tempdir ~/.local/share/paperless/ ~/.local/share/todo/ ~/.local/share/mail
