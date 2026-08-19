991c5575293aca9cb949cdaff41a32c9aaf0c97a
Toni Peter <mail@tonipeter.de>
2026-08-19 10:19:14 +0200

    Open file picker if vim is opened without file

--- scripts/open_vim_in_kitty.sh
+++ scripts/open_vim_in_kitty.sh
@@ -3,5 +3,14 @@
 # Font size for the kitty terminal
 FONT_SIZE=16
 
+if (( $# == 0 )); then
+	project_root="$(git rev-parse --show-toplevel 2>/dev/null || jj root 2>/dev/null)"
+	if [[ -n "$project_root" ]]; then
+		cd "$project_root" || exit
+		kitty --detach -o font_size=$FONT_SIZE nvim -c 'lua Snacks.picker.files()'
+		exit
+	fi
+fi
+
 # Open a new kitty window with nvim
 kitty --detach -o font_size=$FONT_SIZE nvim "$@"
