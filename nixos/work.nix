6d29ab41597b19debc88e652dc0bfbb53f02acc3
Toni Peter <mail@tonipeter.de>
2026-08-31 07:39:26 +0200

    Fix mattermost after update

--- nixos/work.nix
+++ nixos/work.nix
@@ -1,5 +1,15 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [mattermost-desktop vpnc];
+{pkgs, ...}: let
+  mattermost-desktop-fixed = pkgs.symlinkJoin {
+    name = "mattermost-desktop-${pkgs.mattermost-desktop.version}";
+    paths = [pkgs.mattermost-desktop];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/mattermost-desktop \
+        --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib]}
+    '';
+  };
+in {
+  environment.systemPackages = [mattermost-desktop-fixed pkgs.vpnc];
   nixpkgs.config.permittedInsecurePackages = [
     "electron-28.3.3"
   ];
